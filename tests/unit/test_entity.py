5dd5cc770f8f1e718be66c67371a82eff08cef5f
novdov <rpwjd56@gmail.com>
2022-07-28 15:25:08 +0900

    c: entity 필드 순서 테스트 작성

--- tests/unit/test_entity.py
+++ tests/unit/test_entity.py
@@ -5,6 +5,17 @@ import pytest
 from compose.entity import Entity
 
 
+def test_entity_id_field_ordered_first():
+    class TestEntity(Entity):
+        field_a: str
+        field_b: str
+
+    actual = list(TestEntity.__fields__.keys())
+    expected = ["id", "field_a", "field_b", "created_at", "updated_at"]
+
+    assert actual == expected
+
+
 def test_cannot_declare_entity_mismatched_updatable_fields():
     with pytest.raises(ValueError):
 
