b18089fdad9a9ed634fcbd5e6e6708d8e97cda6d
novdov <rpwjd56@gmail.com>
2023-01-06 18:48:49 +0900

    r: Pagination 리팩터링

--- compose/pagination.py
+++ compose/pagination.py
@@ -1,32 +1,34 @@
 import math
 from typing import Any, Optional
 
+from pydantic import conint
+
 from . import container
 
 
 class Pagination(container.BaseModel):
-    total: int
     items: list[Any]
-    page: Optional[int] = None
-    per_page: Optional[int] = None
+    total: int
+    page: Optional[conint(ge=1)] = None
+    per_page: Optional[conint(ge=1)] = None
     extra: Optional[dict[str, Any]] = None
-
-    @property
-    def pages(self) -> int:
-        return math.ceil(self.total / self.per_page) if self.per_page is not None else 1
 
     @property
     def prev_page(self) -> Optional[int]:
         return self.page - 1 if self.has_prev else None
 
     @property
-    def has_prev(self) -> bool:
-        return self.page is not None and self.page > 1
-
-    @property
     def next_page(self) -> Optional[int]:
         return self.page + 1 if self.has_next else None
 
     @property
+    def has_prev(self) -> bool:
+        return self.page is not None and self.page > 1
+
+    @property
     def has_next(self) -> bool:
         return self.page is not None and self.page < self.pages
+
+    @property
+    def pages(self) -> int:
+        return math.ceil(self.total / self.per_page) if self.per_page is not None else 1
