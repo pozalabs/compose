93cf2e7b65f413f28e6727c77ffa9e7ba050c2e1
novdov <rpwjd56@gmail.com>
2023-01-13 14:11:36 +0900

    c: find_by 테스트 작성

---
+++ tests/repository/unit/test_mongo_repository_find_by.py
@@ -0,0 +1,53 @@
+import copy
+from typing import Any
+from unittest import mock
+
+import pytest
+from pymongo.collection import Collection
+
+from compose.entity import Entity
+from compose.repository import MongoRepository
+from compose.types import DateTime, PyObjectId
+
+
+class Model(Entity):
+    name: str
+
+
+@pytest.fixture
+def model_data() -> dict[str, Any]:
+    return dict(
+        id=PyObjectId(b"test-id-0001"),
+        name="name01",
+        created_at=DateTime(2023, 1, 13),
+        updated_at=DateTime(2023, 1, 13),
+    )
+
+
+@pytest.fixture
+def fake_collection(model_data: dict[str, Any]) -> Collection:
+    collection = mock.Mock(spec=Collection)
+    collection.find_one = mock.Mock(return_value=model_data)
+    return collection
+
+
+@pytest.fixture
+def fake_mongo_repository(fake_collection: Collection) -> MongoRepository[Model]:
+    class ModelRepository(MongoRepository[Model]):
+        __collection_name__ = "model"
+
+    return ModelRepository(fake_collection)
+
+
+@pytest.fixture
+def expected(model_data: dict[str, Any]) -> Model:
+    return Model(**copy.deepcopy(model_data))
+
+
+def test_mongo_repository_find_by_return_concrete_entity_type(
+    fake_mongo_repository: MongoRepository[Model],
+    expected: Model,
+):
+    actual = fake_mongo_repository.find_by({"_id": PyObjectId(b"test-id-0001")})
+
+    assert actual == expected
