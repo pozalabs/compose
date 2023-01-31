3ef2af1f088aa597d97869570ed6d5bd75cbf788
novdov <rpwjd56@gmail.com>
2023-01-31 19:41:35 +0900

    u: 테스트 대상이 아닌 클래스명에서 test 제거

--- tests/query/unit/test_mongo_comparison_op.py
+++ tests/query/unit/test_mongo_comparison_op.py
@@ -5,7 +5,7 @@ import pytest
 from compose.query.mongo.op import ComparisonOperator, EmptyOnNull
 
 
-class TestOp(ComparisonOperator):
+class CustomOp(ComparisonOperator):
     def __init__(self, field: str, value: Optional[Any] = None):
         super().__init__(field=field, value=value)
 
@@ -16,8 +16,8 @@ class TestOp(ComparisonOperator):
 @pytest.mark.parametrize(
     "op, expected",
     [
-        (TestOp(field="field", value=None), {}),
-        (TestOp(field="field", value="value"), {"field": "value"}),
+        (CustomOp(field="field", value=None), {}),
+        (CustomOp(field="field", value="value"), {"field": "value"}),
     ],
     ids=(
         "비교 대상이 None인 표현식을 감싸면 빈 표현식 리턴",
