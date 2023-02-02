83826e0f2db55dc0744787870f1840a34c5f41e7
novdov <rpwjd56@gmail.com>
2023-02-03 02:10:48 +0900

    c: Lookup 구현

--- compose/query/mongo/op/stage.py
+++ compose/query/mongo/op/stage.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 import abc
 import functools
 import operator
-from typing import Any, Union
+from typing import Any, ClassVar, Union
 
 from .base import Operator
 from .comparison import ComparisonOperator
 from .logical import And, LogicalOperator, Or
-from .types import DictExpression
+from .types import DictExpression, ListExpression
 
 
 class Stage(Operator):
@@ -60,3 +60,37 @@ class Project(Stage):
     def expression(self) -> DictExpression:
         merged = functools.reduce(operator.or_, [spec.expression() for spec in self.specs])
         return {"$project": merged}
+
+
+class BaseLookup(Stage):
+    alias: ClassVar[dict[str, str]] = {
+        "from_": "from",
+        "as_": "as",
+        "foreign_field": "foreignField",
+        "local_field": "localField",
+    }
+
+    def __init__(self, from_: str, as_: str):
+        self.from_ = from_
+        self.as_ = as_
+
+    def expression(self) -> DictExpression:
+        return {
+            "$lookup": {
+                self.alias.get(field, field): value for field, value in self.__dict__.items()
+            }
+        }
+
+
+class MatchLookup(BaseLookup):
+    def __init__(self, from_: str, as_: str, local_field: str, foreign_field: str):
+        super().__init__(from_=from_, as_=as_)
+        self.local_field = local_field
+        self.foreign_field = foreign_field
+
+
+class SubQueryLookup(BaseLookup):
+    def __init__(self, from_: str, as_: str, let: str, pipeline: ListExpression):
+        super().__init__(from_=from_, as_=as_)
+        self.let = let
+        self.pipeline = pipeline
