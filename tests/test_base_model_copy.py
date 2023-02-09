a1fbb02e1632e26637301acee89c90187e7022fd
novdov <rpwjd56@gmail.com>
2023-02-09 16:54:27 +0900

    u: 선언된 속성 참조 대상 변경

--- tests/test_base_model_copy.py
+++ tests/test_base_model_copy.py
@@ -1,16 +1,41 @@
+from typing import Any
+
 import pytest
 
 from compose.container import BaseModel
 
 
+class Model(BaseModel):
+    name: str
+
+
+class Child(Model):
+    age: int = 10
+
+
 @pytest.fixture
 def model() -> BaseModel:
-    class Model(BaseModel):
-        name: str
-
     return Model(name="name")
 
 
-def test_cannot_copy_with_undefined_fields(model: BaseModel):
+@pytest.fixture
+def child() -> BaseModel:
+    return Child(name="name", age=10)
+
+
+@pytest.mark.parametrize(
+    "update, expected",
+    [
+        (
+            dict(name="name", age=20),
+            Child(name="name", age=20),
+        )
+    ],
+)
+def test_copy_model_with_defaults(child: Child, update: dict[str, Any], expected: Child):
+    assert child.copy(update=update) == expected
+
+
+def test_cannot_copy_with_undefined_fields(model: Model):
     with pytest.raises(AttributeError):
         model.copy(update=dict(name="name", age=10))
