dcc01072e118d9086880903496e96b89768c0512
novdov <rpwjd56@gmail.com>
2023-02-24 15:41:58 +0900

    u: datetime 타입 개선

--- compose/types/datetime.py
+++ compose/types/datetime.py
@@ -1,25 +1,19 @@
 from __future__ import annotations
 
 import datetime
-from typing import Any, Callable, Generator, Union
+from collections.abc import Callable, Generator
+from typing import Any, Union
 
 import pendulum
-from pendulum.tz.timezone import Timezone
 from pydantic.datetime_parse import parse_datetime
 
 
 class DateTime(pendulum.DateTime):
     @classmethod
-    def __get_validators__(cls) -> Generator[Callable[[Any], DateTime], None, None]:
-        yield parse_datetime
-        yield cls._to_dt
+    def __get_validators__(cls) -> Generator[Callable[[Any], pendulum.DateTime], None, None]:
+        yield parse_datetime  # type: ignore
+        yield cls._instance
 
     @classmethod
-    def _to_dt(cls, v: Union[datetime.datetime, pendulum.DateTime]) -> DateTime:
-        if isinstance(v, datetime.datetime):
-            return cls.from_datetime(v)
-        return v
-
-    @classmethod
-    def from_datetime(cls, dt: datetime.datetime, tz: Timezone = pendulum.UTC) -> DateTime:
-        return pendulum.instance(dt=dt, tz=tz)
+    def _instance(cls, v: Union[datetime.datetime, pendulum.DateTime]) -> pendulum.DateTime:
+        return pendulum.instance(dt=v, tz=pendulum.UTC)
