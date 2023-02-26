8ee097699009ea4d3e31f354f655224ea88f6480
novdov <rpwjd56@gmail.com>
2023-02-26 23:56:58 +0900

    u: MongoQuery를 ABC로 설정

--- compose/query/mongo/query.py
+++ compose/query/mongo/query.py
@@ -1,3 +1,4 @@
+import abc
 from typing import Any, Optional
 
 from pydantic import Field
@@ -5,14 +6,19 @@ from pydantic import Field
 from .. import base
 
 
-class MongoQuery(base.Query):
+class MongoQuery(base.Query, abc.ABC):
+    @abc.abstractmethod
     def to_query(self) -> list[dict[str, Any]]:
-        ...
+        raise NotImplementedError
 
 
 class MongoFilterQuery(MongoQuery):
     page: Optional[int] = Field(None, ge=1)
     per_page: Optional[int] = Field(None, ge=1)
+
+    @abc.abstractmethod
+    def to_query(self) -> list[dict[str, Any]]:
+        raise NotImplementedError
 
     @property
     def can_paginate(self) -> bool:
