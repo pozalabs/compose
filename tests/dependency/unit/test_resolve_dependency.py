812a37c02d5452175123c7d32946fb2e0c4d327c
novdov <rpwjd56@gmail.com>
2023-03-10 18:24:36 +0900

    c: resolve_dependency 유닛테스트 작성

---
+++ tests/dependency/unit/test_resolve_dependency.py
@@ -0,0 +1,64 @@
+from typing import Any
+
+import pytest
+from dependency_injector import containers, providers
+
+from compose.dependency import resolve_dependency
+
+
+class Repository:
+    ...
+
+
+class NestedRepository:
+    ...
+
+
+class NestedContainer(containers.DeclarativeContainer):
+    repository = providers.Factory(NestedRepository)
+
+
+class ApplicationContainer(containers.DeclarativeContainer):
+    repository = providers.Factory(Repository)
+
+    nested = providers.Container(NestedContainer)
+
+
+@pytest.mark.parametrize(
+    "type_, container_cls, expected",
+    [
+        (Repository, ApplicationContainer, Repository),
+        (NestedRepository, ApplicationContainer, NestedRepository),
+        (NestedRepository, ApplicationContainer.nested, NestedRepository),
+    ],
+    ids=(
+        "최상위 컨테이너에서 최상위 의존성 해결",
+        "최상위 컨테이너에서 중첩 의존성 해결",
+        "중첩 컨테이너에서 해당 컨테이너에 선언된 의존성 해결",
+    ),
+)
+def test_resolve_dependency(
+    type_: type[Any],
+    container_cls: type[containers.Container],
+    expected: type[Any],
+):
+    resolved = resolve_dependency(type_=type_, container_cls=container_cls)
+
+    assert isinstance(resolved, providers.Factory)
+    assert resolved.cls == expected
+
+
+@pytest.mark.parametrize(
+    "type_, container_cls",
+    [
+        (type("UnregisteredRepository"), ApplicationContainer),
+        (Repository, ApplicationContainer.nested),
+    ],
+    ids=(
+        "등록되지 않은 의존성은 해결할 수 없다",
+        "해당 컨테이너에 선언되지 않은 의존성은 해결할 수 없다",
+    ),
+)
+def test_cannot_resolve_dependency(type_: type[Any], container_cls: type[containers.Container]):
+    with pytest.raises(ValueError):
+        resolve_dependency(type_=type, container_cls=container_cls)
