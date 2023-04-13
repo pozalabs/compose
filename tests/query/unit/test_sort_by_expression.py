8ea71ce58c749ad75c7b03decfb01371b26bdfe0
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-04-13 18:26:09 +0900

    IP-2288: [compose] 정렬 방향별 `SortBy` 팩토리 메서드 구현 (#45)
    * c: 오름차순, 내림차순 방향별 팩토리 메서드 구현
    
    * r: 타입 힌트 강화
    
    * c: asc, desc 표현식 테스트 작성
    
    * c: 정렬 키를 이용하는 팩토리 메서드 구현

---
+++ tests/query/unit/test_sort_by_expression.py
@@ -0,0 +1,26 @@
+import pytest
+
+from compose.query.mongo.op import SortBy
+
+
+def test_asc_expression():
+    assert SortBy.asc("field").expression() == {"field": 1}
+
+
+def test_desc_expression():
+    assert SortBy.desc("field").expression() == {"field": -1}
+
+
+@pytest.mark.parametrize(
+    "key, expected",
+    [
+        ("field", {"field": 1}),
+        ("-field", {"field": -1}),
+    ],
+    ids=(
+        "정렬 키에 접두사가 없으면 오름차순을 적용한다.",
+        "정렬 키에 `-` 접두사가 있으면 내림차순을 적용한다.",
+    ),
+)
+def test_from_expression(key: str, expected: dict[str, int]):
+    assert SortBy.from_(key).expression() == expected
