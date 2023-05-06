2362059dae2697c7bf6e29fb57d2a3fccabb6363
novdov <rpwjd56@gmail.com>
2023-05-06 21:07:00 +0900

    r: aggregation을 패키지로 변경

--- compose/query/mongo/op/aggregation/comparison.py
+++ compose/query/mongo/op/aggregation/comparison.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Any, Optional
+from typing import Any
 
-from . import DictExpression, utils
-from .base import Operator
-from .comparison import ComparisonOperator
+from .. import DictExpression, utils
+from ..base import Operator
+from ..comparison import ComparisonOperator
 
 
 class Expr(Operator):
@@ -33,18 +33,10 @@ def create_aggregation_comparison_operator(
     )
 
 
-class AEqual(ComparisonOperator):
-    def __init__(self, field: str, value: Optional[Any] = None):
-        super().__init__(field=field, value=value)
-
-    def expression(self) -> dict[str, Any]:
-        return {"$eq": [self.field, self.value]}
-
-
 AEq = create_aggregation_comparison_operator(name="AEq", mongo_operator="$eq")
 ANe = create_aggregation_comparison_operator(name="ANe", mongo_operator="$ne")
 AGt = create_aggregation_comparison_operator(name="AGt", mongo_operator="$gt")
 AGte = create_aggregation_comparison_operator(name="AGte", mongo_operator="$gte")
 ALt = create_aggregation_comparison_operator(name="ALt", mongo_operator="$lt")
 ALte = create_aggregation_comparison_operator(name="ALte", mongo_operator="$lte")
-AIn = create_aggregation_comparison_operator(name="AIn", mongo_operator="$in")
+AEqual = AEq
