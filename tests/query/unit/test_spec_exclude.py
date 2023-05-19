900cdbcdb375275cdd0e2399e3b61b4569d32e67
novdov <rpwjd56@gmail.com>
2023-05-19 19:25:53 +0900

    u: expression 테스트로 통합

--- tests/query/unit/test_spec_exclude.py
+++
@@ -1,7 +0,0 @@
-from compose.query.mongo.op import Spec
-
-
-def test_exclude():
-    spec = Spec.exclude("field")
-
-    assert spec.expression() == {"field": 0}
--- tests/query/unit/test_spec_include.py
+++
@@ -1,7 +0,0 @@
-from compose.query.mongo.op import Spec
-
-
-def test_exclude():
-    spec = Spec.include("field")
-
-    assert spec.expression() == {"field": 1}
