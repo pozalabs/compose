4b7a3c62b837373b264102baa8c0849eb53a6e61
novdov <rpwjd56@gmail.com>
2023-08-06 18:57:49 +0900

    c: Cond 구현

--- compose/query/mongo/op/aggregation/conditional.py
+++ compose/query/mongo/op/aggregation/conditional.py
@@ -41,3 +41,26 @@ class IfNull(Operator):
             expressions.append(self.replacement_expr)
 
         return {"$ifNull": [Evaluable(expr).expression() for expr in expressions]}
+
+
+class Cond(Operator):
+    def __init__(self, if_: Any, then: Any, else_: Any):
+        """
+        Aggregation operator `$cond`
+
+        Reference:
+            https://www.mongodb.com/docs/manual/reference/operator/aggregation/cond/#mongodb-expression-exp.-cond
+        """
+
+        self.if_ = if_
+        self.then = then
+        self.else_ = else_
+
+    def expression(self) -> DictExpression:
+        return {
+            "$cond": {
+                "if": Evaluable(self.if_).expression(),
+                "then": Evaluable(self.then).expression(),
+                "else": Evaluable(self.else_).expression(),
+            }
+        }
