222b3565c6c176aa7a3ecebcc0f252dd5271a762
novdov <rpwjd56@gmail.com>
2023-09-14 00:00:31 +0900

    u: schema exclude 테스트 수정

--- tests/schema/unit/test_schema_excludes.py
+++ tests/schema/unit/test_schema_excludes.py
@@ -1,7 +1,9 @@
 from typing import Any, Optional, Type
 
 import pytest
+from pydantic import ConfigDict
 
+import compose
 from compose.container import BaseModel
 from compose.schema.extra import schema_excludes
 
@@ -9,11 +11,11 @@ from compose.schema.extra import schema_excludes
 @pytest.fixture
 def model_type() -> Type[BaseModel]:
     class Model(BaseModel):
+        id: compose.types.PyObjectId
         name: str
         age: Optional[int] = None
 
-        class Config:
-            schema_extra = schema_excludes("age")
+        model_config = ConfigDict(json_schema_extra=schema_excludes("age"))
 
     return Model
 
@@ -21,6 +23,7 @@ def model_type() -> Type[BaseModel]:
 @pytest.fixture
 def expected() -> dict[str, Any]:
     return {
+        "additionalProperties": False,
         "properties": {"name": {"title": "Name", "type": "string"}},
         "required": ["name"],
         "title": "Model",
@@ -29,4 +32,4 @@ def expected() -> dict[str, Any]:
 
 
 def test_schema_by_field_name(model_type: Type[BaseModel], expected: dict[str, Any]):
-    assert model_type.schema() == expected
+    assert model_type.model_json_schema() == expected
