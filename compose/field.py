ce25da0a55f4ba75043a6c70f10673e81993a67d
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-09-25 15:23:16 +0900

    u: Field.__call__ 모든 인자를 kwargs 형태로 변경

--- compose/field.py
+++ compose/field.py
@@ -8,13 +8,15 @@ from . import types
 
 
 class _IdField:
-    def __call__(self, alias: str = "_id", **kwargs) -> Any:
-        return Field(alias=alias, **kwargs)  # type: ignore
+    def __call__(self, **kwargs) -> Any:
+        default_kwargs = {"alias": "_id"}
+        return Field(**(default_kwargs | kwargs))  # type: ignore
 
 
 class _DatetimeField:
     def __call__(self, **kwargs) -> types.DateTime:
-        return Field(default_factory=pendulum.DateTime.utcnow, **kwargs)  # type: ignore
+        default_kwargs = {"default_factory": pendulum.DateTime.utcnow}
+        return Field(**(default_kwargs | kwargs))  # type: ignore
 
 
 IdField: Callable[..., Any] = _IdField()
