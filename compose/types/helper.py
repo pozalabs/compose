9555271b7091bdac04d37ba7fc16b6b3cf15ad98
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-09-27 23:56:50 +0900

    u: 클래스 대신 CoreSchema를 리턴하는 변환 함수 작성

--- compose/types/helper.py
+++ compose/types/helper.py
@@ -1,27 +1,46 @@
-from collections.abc import Callable
-from typing import Any
+import copy
+from collections.abc import Callable, Generator
+from typing import Any, Protocol
 
-from pydantic import GetCoreSchemaHandler
-from pydantic_core import CoreSchema, core_schema
+from pydantic_core import core_schema
 
 
-class Validatable:
+class SupportsGetValidators(Protocol):
+    """Naming Convention: https://github.com/python/typeshed/issues/4174"""
+
     @classmethod
-    def __get_pydantic_core_schema__(
-        cls, source_type: Any, handler: GetCoreSchemaHandler
-    ) -> CoreSchema:
-        get_validators = getattr(cls, "__get_validators__", lambda: [])
-        return core_schema.no_info_after_validator_function(
-            create_chain(*get_validators()),
-            handler(cls.__mro__[-2]),
-        )
+    def __get_validators__(cls) -> Generator[[Callable[[Any], Any]], None, None]:
+        ...
 
 
-def create_chain(*_validators: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    def chain(_v: Any) -> Any:
-        _result = _v
-        for _validator in _validators:
-            _result = _validator(_result)
-        return _result
+def get_pydantic_core_schema(
+    t: SupportsGetValidators, schema: core_schema.CoreSchema, /
+) -> core_schema.CoreSchema:
+    """Pydantic v1 커스텀 타입에 구현한 검증 제너레이터를 v2 커스텀 타입 검증 함수로 변환합니다.
 
-    return chain
+    class CustomType(str):
+        @classmethod
+        def __get_validators__(cls):
+            yield str_validator
+
+        @classmethod
+        def __get_pydantic_core_schema__(
+            cls, source_type: Any, handler: GetCoreSchemaHandler
+        ) -> core_schema.CoreSchema:
+            return compose.types.get_pydantic_core_schema(cls, handler(str))
+    """
+
+    if not hasattr(t, "__get_validators__"):
+        raise AttributeError(f"{t.__class__.__name__} does not have `__get_validators__` method")
+
+    return core_schema.no_info_after_validator_function(chain(*t.__get_validators__()), schema)
+
+
+def chain(*validators: Callable[[Any], Any]) -> Callable[[Any], Any]:
+    def apply_chain(v: Any) -> Any:
+        result = copy.deepcopy(v)
+        for validator in validators:
+            result = validator(result)
+        return result
+
+    return apply_chain
