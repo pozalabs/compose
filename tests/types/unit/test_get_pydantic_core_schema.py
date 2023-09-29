dba99f435a5f645cc8489a065c6c34b0c3d1c2fb
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-09-29 12:10:39 +0900

    IP-3581: [compose] pydantic v1 커스텀 타입 검증기를 v2 커스텀 타입 검증 함수로 변환하는 함수 구현 (#82)
    * c: __get_pydantic_core_schema__를 구현하는 Validatable 구현
    
    * u: 클래스 대신 CoreSchema를 리턴하는 변환 함수 작성
    
    * c: get_pydantic_core_schema 유닛 테스트 작성

---
+++ tests/types/unit/test_get_pydantic_core_schema.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from pydantic import GetCoreSchemaHandler, TypeAdapter
+from pydantic.v1.validators import str_validator
+from pydantic_core import core_schema
+
+from compose.types import get_pydantic_core_schema
+
+
+class StrippedStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield str_validator
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v: str) -> str:
+        return v.strip()
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        return get_pydantic_core_schema(cls, handler(str))
+
+
+def test_get_pydantic_core_schema():
+    validated_type = TypeAdapter(StrippedStr).validate_python(" test ")
+
+    assert validated_type == StrippedStr("test")
