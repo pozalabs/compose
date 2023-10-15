d2593baa6a5a0512279c39246735ea32cb3b50cf
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-10-15 16:54:01 +0900

    u: 버전 참조 수정

--- tests/types/unit/test_get_pydantic_core_schema.py
+++ tests/types/unit/test_get_pydantic_core_schema.py
@@ -2,12 +2,13 @@ from typing import Any
 
 import pytest
 from pydantic import GetCoreSchemaHandler, TypeAdapter
-from pydantic.validators import str_validator
 from pydantic_core import core_schema
 
 from compose import compat
 
 if compat.IS_PYDANTIC_V2:
+    from pydantic.v1.validators import str_validator
+
     from compose.types import get_pydantic_core_schema
 
 
