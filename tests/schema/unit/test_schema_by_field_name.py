55e6533cbb05a97ddc849f1c7afd4405e4381575
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-10-15 17:27:48 +0900

    u: schema 테스트 호환성 보완

--- tests/schema/unit/test_schema_by_field_name.py
+++ tests/schema/unit/test_schema_by_field_name.py
@@ -1,18 +1,26 @@
-from typing import Any, Type
+from typing import Any
 
 import pytest
-from pydantic import ConfigDict, Field
+from pydantic import Field
 
-from compose.container import BaseModel
+import compose
 from compose.schema.extra import schema_by_field_name
+
+if compose.compat.IS_PYDANTIC_V2:
+    from pydantic import ConfigDict
 
 
 @pytest.fixture
-def model_type() -> Type[BaseModel]:
-    class Model(BaseModel):
+def model_type() -> type[compose.BaseModel]:
+    class Model(compose.BaseModel):
         name: str = Field(alias="username")
 
-        model_config = ConfigDict(json_schema_extra=schema_by_field_name())
+        if compose.compat.IS_PYDANTIC_V2:
+            model_config = ConfigDict(json_schema_extra=schema_by_field_name())
+        else:
+
+            class Config:
+                json_schema_extra = schema_by_field_name()
 
     return Model
 
@@ -27,5 +35,5 @@ def expected() -> dict[str, Any]:
     }
 
 
-def test_schema_by_field_name(model_type: Type[BaseModel], expected: dict[str, Any]):
-    assert model_type.model_json_schema() == expected
+def test_schema_by_field_name(model_type: type[compose.BaseModel], expected: dict[str, Any]):
+    assert compose.compat.model_schema(model_type) == expected
