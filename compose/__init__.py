35e662114165d51cdcffa519e0fcfc042a4f4c37
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-14 00:46:10 +0900

    u: event 모듈 구현 통합

--- compose/__init__.py
+++ compose/__init__.py
@@ -1,10 +1,10 @@
-from . import compat, db, dependency, entity, pagination, repository, result, types
+from . import compat, db, dependency, entity, event, pagination, repository, result, types
 from .container import BaseModel, TimeStampedModel
 
 if compat.IS_PYDANTIC_V2:
-    from . import command, event, query, schema
+    from . import command, query, schema
 else:
-    from .v1 import command, event, query, schema
+    from .v1 import command, query, schema
 
 __all__ = [
     "BaseModel",
