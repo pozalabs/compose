94722b7ca9d44e4b6afda56ab79be4f73b59241f
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-14 00:46:37 +0900

    u: command 모듈 구현 통합

--- compose/__init__.py
+++ compose/__init__.py
@@ -1,10 +1,10 @@
-from . import compat, db, dependency, entity, event, pagination, repository, result, types
+from . import command, compat, db, dependency, entity, event, pagination, repository, result, types
 from .container import BaseModel, TimeStampedModel
 
 if compat.IS_PYDANTIC_V2:
-    from . import command, query, schema
+    from . import query, schema
 else:
-    from .v1 import command, query, schema
+    from .v1 import query, schema
 
 __all__ = [
     "BaseModel",
