9dcf0b925ec9ecbebf9cb1d3b8632b15869c6c56
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-15 02:57:43 +0900

    u: with_transaction에 session/transaction 옵션 주입

--- compose/db/mongo.py
+++ compose/db/mongo.py
@@ -1,34 +1,34 @@
 from collections.abc import Callable
-from typing import TypeAlias, TypeVar
+from typing import TypeVar
 
-from pymongo import WriteConcern
-from pymongo.client_session import ClientSession
-from pymongo.read_concern import ReadConcern
-from pymongo.read_preferences import Nearest, Primary, PrimaryPreferred, SecondaryPreferred
+from pymongo import MongoClient
+from pymongo.client_session import ClientSession, SessionOptions, TransactionOptions
 
 T = TypeVar("T")
-ServerMode: TypeAlias = Primary | PrimaryPreferred | SecondaryPreferred | Nearest
 
 
 class MongoUnitOfWork:
     def __init__(self, session_factory: Callable[..., ClientSession]):
         self.session_factory = session_factory
+        MongoClient().start_session()
 
     def with_transaction(
         self,
         callback: Callable[[ClientSession], T],
-        read_concern: ReadConcern | None = None,
-        write_concern: WriteConcern | None = None,
-        read_preference: ServerMode | None = None,
-        max_commit_time_ms: int | None = None,
+        session_options: SessionOptions | None = None,
+        transaction_options: TransactionOptions | None = None,
     ) -> T:
-        with self.session_factory() as session:
+        with self.session_factory(
+            causal_consistency=session_options.causal_consistency,
+            default_transaction_options=session_options.default_transaction_options,
+            snapshot=session_options.snapshot,
+        ) as session:
             result = session.with_transaction(
                 callback=callback,
-                read_concern=read_concern,
-                write_concern=write_concern,
-                read_preference=read_preference,
-                max_commit_time_ms=max_commit_time_ms,
+                read_concern=transaction_options.read_concern,
+                write_concern=transaction_options.write_concern,
+                read_preference=transaction_options.read_preference,
+                max_commit_time_ms=transaction_options.max_commit_time_ms,
             )
 
         return result
