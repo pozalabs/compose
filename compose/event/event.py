8ce8c7278df25a8388c968f84dcff14b29e6da38
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-19 23:53:28 +0900

    u: default_factory 지정

--- compose/event/event.py
+++ compose/event/event.py
@@ -2,5 +2,5 @@ from .. import container, field, types
 
 
 class Event(container.BaseModel):
-    id: types.PyObjectId = field.IdField()
+    id: types.PyObjectId = field.IdField(default_factory=types.PyObjectId)
     published_at: types.DateTime = field.DateTimeField()
