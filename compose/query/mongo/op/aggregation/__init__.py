1a3518fa20895abfbb86d6df57ce253f35e8cb01
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-25 23:12:54 +0900

    r: expression 모듈명 변경

--- compose/query/mongo/op/aggregation/__init__.py
+++ compose/query/mongo/op/aggregation/__init__.py
@@ -1,5 +1,5 @@
 from .array import *  # noqa: F401, F403
+from .array_expression import *  # noqa: F401, F403
 from .comparison import *  # noqa: F401, F403
 from .conditional import *  # noqa: F401, F403
-from .expression import *  # noqa: F401, F403
 from .string import *  # noqa: F401, F403
