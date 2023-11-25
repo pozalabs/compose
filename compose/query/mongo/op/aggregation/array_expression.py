662c48992c1fe00f08d48b0979ea974bee8ae7d5
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-25 23:24:21 +0900

    c: Filter 구현

--- compose/query/mongo/op/aggregation/array_expression.py
+++ compose/query/mongo/op/aggregation/array_expression.py
@@ -26,3 +26,22 @@ class Size(Operator):
 
     def expression(self) -> DictExpression:
         return {"$size": Evaluable(self.expression).expression()}
+
+
+class Filter(Operator):
+    def __init__(self, input_: Any, as_: str, cond: Any, limit: Any | None = None):
+        self.input = input_
+        self.as_ = _String(as_)
+        self.cond = cond
+        self.limit = limit
+
+    def expression(self) -> DictExpression:
+        expression = {
+            "input": Evaluable(self.input).expression(),
+            "as": self.as_,
+            "cond": Evaluable(self.cond).expression(),
+        }
+        if self.limit is not None:
+            expression["limit"] = Evaluable(self.limit).expression()
+
+        return {"$filter": expression}
