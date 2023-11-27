a4ea68c020fd0481b56633cc95d1756f7166b3ca
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-11-28 01:09:52 +0900

    u: SqsMessageQueue를 선택 의존성으로 추가

--- compose/messaging/__init__.py
+++ compose/messaging/__init__.py
@@ -2,14 +2,20 @@ from .consumer import MessageConsumer
 from .consumer_runner import MessageConsumerThreadRunner
 from .messagebus import MessageBus
 from .model import EventMessage, SqsEventMessage
-from .queue import MessageQueue, SqsMessageQueue
+from .queue import MessageQueue
 
 __all__ = [
     "EventMessage",
     "SqsEventMessage",
     "MessageQueue",
-    "SqsMessageQueue",
     "MessageConsumer",
     "MessageConsumerThreadRunner",
     "MessageBus",
 ]
+
+try:
+    from .queue import SqsMessageQueue  # noqa: F401
+
+    __all__.append("SqsMessageQueue")
+except ImportError:
+    pass
