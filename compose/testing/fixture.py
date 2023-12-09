0c683fe8febd91d11cff3caa21a88cc0d1f4d70d
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-12-09 14:40:00 +0900

    u: getfixturevalue 래핑 함수 구현

--- compose/testing/fixture.py
+++ compose/testing/fixture.py
@@ -5,3 +5,7 @@ import pytest
 
 def get_fixture_param(request: pytest.FixtureRequest, default: Any | None = None) -> Any:
     return getattr(request, "param", default)
+
+
+def get_fixture_value_from_param(request: pytest.FixtureRequest) -> Any:
+    return request.getfixturevalue(get_fixture_param(request))
