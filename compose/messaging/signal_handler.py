e98d2f63c3c59f925c9144084f0ad39c522f896b
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-12-30 21:37:47 +0900

    u: 메시지를 수신했을 때만 on_receive 훅 실행

---
+++ compose/messaging/signal_handler.py
@@ -0,0 +1,40 @@
+import abc
+import logging
+import threading
+
+logger = logging.getLogger("compose")
+
+
+class SignalHandler(abc.ABC):
+    @abc.abstractmethod
+    def handle(self) -> None:
+        raise NotImplementedError
+
+    @property
+    def received_signal(self) -> bool:
+        raise NotImplementedError
+
+
+class DefaultSignalHandler(SignalHandler):
+    def __init__(self):
+        self._received_signal = False
+
+    def handle(self) -> None:
+        self._received_signal = True
+
+    @property
+    def received_signal(self) -> bool:
+        return self._received_signal
+
+
+class ThreadSignalHandler(SignalHandler):
+    def __init__(self):
+        self._exit_event = threading.Event()
+
+    def handle(self) -> None:
+        self._exit_event.set()
+        print(self._exit_event.is_set())
+
+    @property
+    def received_signal(self) -> bool:
+        return self._exit_event.is_set()
