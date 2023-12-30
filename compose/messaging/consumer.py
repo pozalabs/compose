6d4d024ed7e84e45243f704d9498c11a47a23db7
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2023-12-30 22:30:25 +0900

    IP-3923: [compose] 멀티스레드에서의 MessageConsumer 시그널 핸들링 적용 오류 수정 (#147)
    * u: 메시지를 수신했을 때만 on_receive 훅 실행
    
    * d: 불필요한 print 제거
    
    * u: 메시지 소보 에러 메시지 개선
    
    * u: ThreadSignalHandler 노출
    
    * u: MessageConsumer.run 인자에 SignalHandler 주입
    
    * u: MessageConsumerThreadRunner에 시그널 핸들링 적용
    
    * u: signal_handler_factory 인자 기본값 적용
    
    * u: signal_handler를 MessageConsumer 인스턴스 인자로 주입
    
    * u: MessageConsumerThreadRunner 이름 변경
    
    * Revert "u: MessageConsumerThreadRunner 이름 변경"
    
    This reverts commit b5ffe82497d9fb34fb7703b6146209aee352c610.

--- compose/messaging/consumer.py
+++ compose/messaging/consumer.py
@@ -2,12 +2,12 @@ import asyncio
 import json
 import logging
 from collections.abc import Callable
-from typing import Literal, Protocol, TypeAlias
+from typing import Literal, TypeAlias
 
-from .._signal import SignalHandler
 from . import model
 from .messagebus import MessageBus
 from .queue import MessageQueue
+from .signal_handler import DefaultSignalHandler, SignalHandler
 
 HookEventType = Literal[
     "on_start",
@@ -15,17 +15,12 @@ HookEventType = Literal[
     "on_receive_error",
     "on_consume",
     "on_consume_error",
+    "on_shutdown",
 ]
 HookArgType: TypeAlias = str | model.EventMessage | Exception
-Hook = Callable[[HookArgType], None]
+Hook: TypeAlias = Callable[[HookArgType], None]
 
 logger = logging.getLogger("compose")
-
-
-class SignalHandleable(Protocol):
-    @property
-    def received_signal(self) -> bool:
-        return ...
 
 
 def log_event_message(log_message: str, message: model.EventMessage) -> None:
@@ -44,7 +39,8 @@ DEFAULT_HOOKS = {
     "on_receive": [lambda message: log_event_message("Received message", message)],
     "on_receive_error": [lambda exc: log_exception("Failed to receive message", exc)],
     "on_consume": [lambda message: log_event_message("Consumed message", message)],
-    "on_consume_error": [lambda exc: log_exception("Failed to consume message", exc)],
+    "on_consume_error": [lambda exc: log_exception(f"Failed to consume message due to {exc}", exc)],
+    "on_shutdown": [logger.info],
 }
 
 
@@ -54,12 +50,12 @@ class MessageConsumer:
         messagebus: MessageBus,
         message_queue: MessageQueue,
         hooks: dict[HookEventType, list[Hook]] | None = None,
-        signal_handler: SignalHandleable | None = None,
+        signal_handler: SignalHandler | None = None,
     ):
         self.messagebus = messagebus
         self.message_queue = message_queue
         self.hooks = DEFAULT_HOOKS | (hooks or {})
-        self.signal_handler = signal_handler or SignalHandler()
+        self.signal_handler = signal_handler or DefaultSignalHandler()
 
         self._default_hook = lambda _: None
 
@@ -73,9 +69,9 @@ class MessageConsumer:
                 self._execute_hook("on_receive_error", exc)
                 continue
 
-            self._execute_hook("on_receive", message)
             if message is None:
                 continue
+            self._execute_hook("on_receive", message)
 
             try:
                 await asyncio.create_task(self.consume(message))
@@ -91,3 +87,7 @@ class MessageConsumer:
     def _execute_hook(self, hook_event_type: HookEventType, arg: HookArgType, /) -> None:
         for hook in self.hooks.get(hook_event_type, [self._default_hook]):
             hook(arg)
+
+    def shutdown(self) -> None:
+        self._execute_hook("on_shutdown", "MessageConsumer shutting down")
+        self.signal_handler.handle()
