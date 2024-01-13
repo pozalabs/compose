ccbdcd7ca4d1f6b72d13c4e89979c03a87ac406f
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-01-13 18:13:13 +0900

    IP-4026: [compose] Evaluable 연산자가 중첩 표현식을 모두 평가하도록 수정 (#150)
    * u: Evaluable에서 평가 가능한 표현식을 모두 중첩 평가
    
    * u: 중첩 표현식 평가 테스트 케이스 추가
    
    * r: 테스트 이름 개선
    
    * u: Raw 입력 표현식을 Evaluable로 평가

--- tests/query/unit/test_evaluable_expression.py
+++ tests/query/unit/test_evaluable_expression.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from compose.query.mongo.op import Evaluable, Spec
+from compose.query.mongo.op import AEq, Evaluable, Filter, Raw, Spec
 
 
 @pytest.mark.parametrize(
@@ -20,11 +20,49 @@ from compose.query.mongo.op import Evaluable, Spec
             Evaluable("$field"),
             "$field",
         ),
+        (
+            Evaluable(
+                {
+                    "$mergeObjects": [
+                        "$$tag",
+                        Spec(
+                            field="keyword",
+                            spec=Raw(
+                                {
+                                    "$first": Filter(
+                                        input_="$keywords",
+                                        as_="keyword",
+                                        cond=AEq("$$keyword._id", "$$tag.keyword_id"),
+                                    )
+                                }
+                            ),
+                        ),
+                    ]
+                }
+            ),
+            {
+                "$mergeObjects": [
+                    "$$tag",
+                    {
+                        "keyword": {
+                            "$first": {
+                                "$filter": {
+                                    "input": "$keywords",
+                                    "as": "keyword",
+                                    "cond": {"$eq": ["$$keyword._id", "$$tag.keyword_id"]},
+                                }
+                            }
+                        }
+                    },
+                ]
+            },
+        ),
     ],
     ids=(
         "`Operator`를 입력하면 평가한 표현식을 리턴한다.",
         "딕셔너리를 입력하면 값을 그대로 리턴한다",
         "문자열을 입력하면 값을 그대로 리턴한다",
+        "원시 표현식과 `Operator`로 이루어진 중첩 평가식을 중첩문 가장 아래까지 모두 평가한다.",
     ),
 )
 def test_expression(op: Evaluable, expected: Any):
