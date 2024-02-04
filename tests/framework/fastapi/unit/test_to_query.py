43489fdef7c8ea5a02182721ef45af373d215b17
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-02-04 19:00:14 +0900

    IP-4205: [compose] 시퀀스 필드를 선언한 쿼리 클래스를 FastAPI 클래스 의존성으로 주입 가능한 클래스로 변환하는 기능 구현 (#159)
    * c: to_query 함수 구현
    
    * u: 모델 생성 오류 수정
    
    * c: to_query 테스트 작성
    
    * u: fastapi 패키지 하위 모듈 노출
    
    * d: 사용하지 않는 변수 제거
    
    * u: pydantic v1에서 Query를 인식할 수 있게 변경
    
    * u: config 선언 방법 수정
    
    * u: config 주입 개선
    
    * u: 신규 클래스 이름 변경
    
    * u: pydantic v1 실행 오류 수정
    
    * u: fastapi 패키지를 최상위에 노출

---
+++ tests/framework/fastapi/unit/test_to_query.py
@@ -0,0 +1,39 @@
+import http
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.testclient import TestClient
+from pydantic import Field
+
+import compose
+from compose import compat
+from compose.framework.fastapi import to_query
+
+app = FastAPI()
+
+
+class ListItems(compose.query.Query):
+    types: list[str] | None = Field(None, alias="type")
+    page: int = 1
+    per_page: int = 10
+
+
+@app.get("/items")
+def get(q: Annotated[ListItems, Depends(to_query(ListItems))]):
+    return compat.model_dump(q)
+
+
+client = TestClient(app)
+
+
+def test_to_query():
+    response = client.get("/items?type=foo&type=bar&page=2&per_page=20")
+
+    expected_response = {
+        "types": ["foo", "bar"],
+        "page": 2,
+        "per_page": 20,
+    }
+
+    assert response.status_code == http.HTTPStatus.OK
+    assert response.json() == expected_response
