3e5592a740604c2ddf2365cc21b8822670641ac0
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-02-10 17:52:28 +0900

    u: ToString 위치 변경

--- compose/query/mongo/op/aggregation/type_expression.py
+++ compose/query/mongo/op/aggregation/type_expression.py
@@ -4,6 +4,14 @@ from ..base import Evaluable, Operator
 from ..types import DictExpression
 
 
+class ToString(Operator):
+    def __init__(self, expr: Any):
+        self.expr = expr
+
+    def expression(self) -> DictExpression:
+        return Evaluable({"$toString": self.expr}).expression()
+
+
 class ToBool(Operator):
     def __init__(self, expression: Any):
         self._expression = expression
