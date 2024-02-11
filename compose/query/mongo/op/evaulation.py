198ee2e6f04560f8c2e6a6beca4a185ad888e361
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-02-12 00:51:03 +0900

    IP-4246: [compose] Expr 표현식 생성 오류 수정 (#173)
    * u: Expr에서 모든 표현식 허용
    
    * u: expr 연산자 누락 오류 수정
    
    * u: RegexMatch에서 Evaluable 사용

--- compose/query/mongo/op/evaulation.py
+++ compose/query/mongo/op/evaulation.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from .base import Evaluable, Operator
 from .types import DictExpression
 
 
 class Expr(Operator):
-    def __init__(self, op: Operator):
+    def __init__(self, op: Any):
         self.op = op
 
     def expression(self) -> DictExpression:
-        return Evaluable(self.op).expression()
+        return Evaluable({"$expr": self.op}).expression()
