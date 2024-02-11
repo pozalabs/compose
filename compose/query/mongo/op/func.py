cbc4aedd4619885afa80a602e265f2df1ab72445
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-02-12 01:32:49 +0900

    c: Filter 구현

--- compose/query/mongo/op/func.py
+++ compose/query/mongo/op/func.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Iterable
-from typing import TypeVar
+from typing import Self, TypeVar
 
 from .base import Operator
 
@@ -16,4 +16,21 @@ class _Map:
 
 
 def Map(collection: Iterable[T], callback: Callable[[T], Operator]) -> list[Operator]:  # noqa: N802
-    return _Map(collection, callback)()
+    return _Map(collection, callback).eval()
+
+
+class _Filter:
+    def __init__(self, collection: list[Operator], predicate: Callable[[Operator], bool]):
+        self.collection = collection
+        self.predicate = predicate
+
+    def eval(self) -> list[Operator]:
+        return [item for item in self.collection if self.predicate(item)]
+
+    @classmethod
+    def non_empty(cls, *ops: Operator) -> Self:
+        return cls(*ops, predicate=lambda op: op.expression())
+
+
+def Filter(*ops: Operator, predicate: Callable[[Operator], bool]) -> list[Operator]:  # noqa: N802
+    return _Filter(*ops, predicate=predicate).eval()
