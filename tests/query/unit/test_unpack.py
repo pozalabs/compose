d4954b1479835d6a492b3c3b0923292af2c4a333
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-02-11 22:44:41 +0900

    c: Unpack 테스트 작성

---
+++ tests/query/unit/test_unpack.py
@@ -0,0 +1,49 @@
+import pytest
+
+from compose.query.mongo.op import (
+    Eq,
+    ListExpression,
+    Match,
+    MatchLookup,
+    Pipeline,
+    Set,
+    Spec,
+    Unpack,
+)
+
+
+@pytest.mark.parametrize(
+    "op,expected",
+    [
+        (
+            Unpack(
+                Match.and_(
+                    Eq(field="field", value="value"),
+                ),
+                Pipeline(
+                    MatchLookup(
+                        from_="from",
+                        local_field="local_field",
+                        foreign_field="foreign_field",
+                        as_="as",
+                    ),
+                    Set(Spec(field="new_field", spec="new_value")),
+                ),
+            ),
+            [
+                {"$match": {"$and": [{"field": {"$eq": "value"}}]}},
+                {
+                    "$lookup": {
+                        "from": "from",
+                        "localField": "local_field",
+                        "foreignField": "foreign_field",
+                        "as": "as",
+                    }
+                },
+                {"$set": {"new_field": "new_value"}},
+            ],
+        ),
+    ],
+)
+def test_expression(op: Unpack, expected: ListExpression):
+    assert op.expression() == expected
