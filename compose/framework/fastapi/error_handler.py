36828e048e28d799bfdd5be8209d13022fa71d3f
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-03-01 17:28:39 +0900

    u: Reponse 타입을 외부에서 결정하게 허용

--- compose/framework/fastapi/error_handler.py
+++ compose/framework/fastapi/error_handler.py
@@ -1,3 +1,4 @@
+import functools
 import http
 from collections.abc import Callable
 from typing import Self, TypeAlias
@@ -5,7 +6,6 @@ from typing import Self, TypeAlias
 from fastapi import Request, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException, RequestValidationError
-from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 
 from compose import compat, schema
@@ -19,15 +19,25 @@ class ErrorHandlerInfo(BaseModel):
     handler: ErrorHandler
 
     @classmethod
-    def for_status_code(cls, status_code: int, error_type: str) -> Self:
+    def for_status_code(
+        cls, status_code: int, error_type: str, response_cls: type[Response]
+    ) -> Self:
         return cls(
             exc_class_or_status_code=status_code,
-            handler=create_error_handler(status_code=status_code, error_type=error_type),
+            handler=create_error_handler(
+                status_code=status_code,
+                error_type=error_type,
+                response_cls=response_cls,
+            ),
         )
 
     @classmethod
-    def from_status_code(cls, status_code: http.HTTPStatus) -> Self:
-        return cls.for_status_code(status_code=status_code, error_type=status_code.name.lower())
+    def from_status_code(cls, status_code: http.HTTPStatus, response_cls: type[Response]) -> Self:
+        return cls.for_status_code(
+            status_code=status_code,
+            error_type=status_code.name.lower(),
+            response_cls=response_cls,
+        )
 
     @classmethod
     def for_exc(
@@ -35,35 +45,47 @@ class ErrorHandlerInfo(BaseModel):
         exc_type: type[Exception],
         status_code: int,
         error_type: str,
+        response_cls: type[Response],
     ) -> Self:
         return cls(
             exc_class_or_status_code=exc_type,
-            handler=create_error_handler(status_code=status_code, error_type=error_type),
+            handler=create_error_handler(
+                status_code=status_code,
+                error_type=error_type,
+                response_cls=response_cls,
+            ),
         )
 
     @classmethod
-    def for_http_exception(cls, exc: HTTPException) -> Self:
-        return cls.from_status_code(http.HTTPStatus(exc.status_code))
+    def for_http_exception(cls, exc: HTTPException, response_cls: type[Response]) -> Self:
+        return cls.from_status_code(
+            status_code=http.HTTPStatus(exc.status_code),
+            response_cls=response_cls,
+        )
 
     @classmethod
-    def request_validation_error(cls) -> Self:
+    def request_validation_error(cls, response_cls: type[Response]) -> Self:
         return cls(
             exc_class_or_status_code=RequestValidationError,
-            handler=validation_exception_handler,
+            handler=create_validation_error_handler(response_cls),
         )
 
     @classmethod
-    def pydantic_validation_error(cls) -> Self:
+    def pydantic_validation_error(cls, response_cls: type[Response]) -> Self:
         return cls(
             exc_class_or_status_code=ValidationError,
-            handler=validation_exception_handler,
+            handler=create_validation_error_handler(response_cls),
         )
 
 
-def create_error_handler(status_code: int, error_type: str) -> ErrorHandler:
+def create_error_handler(
+    status_code: int,
+    error_type: str,
+    response_cls: type[Response],
+) -> ErrorHandler:
     def error_handler(request: Request, exc: Exception) -> Response:
         if isinstance(exc, HTTPException):
-            return JSONResponse(
+            return response_cls(
                 content=jsonable_encoder(getattr(exc, "detail")),
                 status_code=exc.status_code,
             )
@@ -80,14 +102,20 @@ def create_error_handler(status_code: int, error_type: str) -> ErrorHandler:
                 ]
             ),
         )
-        return JSONResponse(content=jsonable_encoder(response), status_code=status_code)
+        return response_cls(content=jsonable_encoder(response), status_code=status_code)
 
     return error_handler
 
 
+def create_validation_error_handler(response_cls: type[Response]) -> ErrorHandler:
+    return functools.partial(validation_exception_handler, response_cls=response_cls)
+
+
 def validation_exception_handler(
-    request: Request, exc: RequestValidationError | ValidationError
-) -> JSONResponse:
+    request: Request,
+    exc: RequestValidationError | ValidationError,
+    response_cls: type[Response],
+) -> Response:
     response = schema.Error(
         title="validation_error",
         type="validation_error",
@@ -100,7 +128,7 @@ def validation_exception_handler(
             for error in exc.errors()
         ],
     )
-    return JSONResponse(
+    return response_cls(
         content=jsonable_encoder(response),
         status_code=http.HTTPStatus.UNPROCESSABLE_ENTITY,
     )
