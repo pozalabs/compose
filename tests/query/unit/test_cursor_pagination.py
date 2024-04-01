bfbf0c140ec0256b939ea88844ff852a8ccaf3b1
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-04-01 10:46:13 +0900

    c: CursorPagination 표현식 테스트 작성

---
+++ tests/query/unit/test_cursor_pagination.py
@@ -0,0 +1,41 @@
+import pendulum
+import pytest
+
+from compose.query.mongo.op import (
+    CursorDecoder,
+    CursorEncoder,
+    CursorPagination,
+    ListExpression,
+    Sort,
+    SortBy,
+)
+
+
+@pytest.mark.parametrize(
+    "pagination, expected",
+    [
+        (
+            CursorPagination(
+                sort=Sort(SortBy.desc(field="created_at")),
+                cursor_decoder=CursorDecoder(parsers={"created_at": pendulum.parse}),
+                cursor=CursorEncoder(cursor_keys=["created_at"]).encode(
+                    {"created_at": pendulum.datetime(2024, 4, 1).isoformat()}
+                ),
+                per_page=10,
+            ),
+            [
+                {
+                    "$match": {
+                        "$nor": [
+                            {"created_at": {"$lt": pendulum.datetime(2024, 4, 1)}},
+                        ]
+                    }
+                },
+                {"$sort": {"created_at": -1}},
+                {"$limit": 10},
+            ],
+        )
+    ],
+)
+def test_expression(pagination: CursorPagination, expected: ListExpression):
+    assert pagination.expression() == expected
