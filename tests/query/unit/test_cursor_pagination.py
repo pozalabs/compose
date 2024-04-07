fffd3b41f787283e3344054f8bc1189242bbc8fa
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-04-07 17:42:24 +0900

    c: CursorPaginationClause 테스트 작성

--- tests/query/unit/test_cursor_pagination.py
+++ tests/query/unit/test_cursor_pagination.py
@@ -5,6 +5,8 @@ import compose
 from compose.query.mongo.op import (
     Cursor,
     CursorPagination,
+    CursorPaginationClause,
+    DictExpression,
     ListExpression,
     Sort,
     SortBy,
@@ -23,6 +25,51 @@ def test_cursor_encode_decode():
     )
 
     assert TCursor.from_str(cursor.to_str()) == cursor
+
+
+@pytest.mark.parametrize(
+    "clause, expected",
+    [
+        (
+            CursorPaginationClause.from_cursor_params(
+                sort_field="created_at",
+                cursor_params=[("created_at", pendulum.datetime(2024, 4, 1))],
+                sort=Sort(
+                    SortBy.asc("created_at"),
+                ),
+            ),
+            {"$and": [{"created_at": {"$gt": pendulum.datetime(2024, 4, 1)}}]},
+        ),
+        (
+            CursorPaginationClause.from_cursor_params(
+                sort_field="_id",
+                cursor_params=[
+                    ("created_at", pendulum.datetime(2024, 4, 1)),
+                    ("_id", compose.types.PyObjectId(b"test-id-0001")),
+                ],
+                sort=Sort(
+                    SortBy.asc("created_at"),
+                    SortBy.desc("_id"),
+                ),
+            ),
+            {
+                "$and": [
+                    {"created_at": {"$eq": pendulum.datetime(2024, 4, 1)}},
+                    {"_id": {"$lt": compose.types.PyObjectId(b"test-id-0001")}},
+                ]
+            },
+        ),
+    ],
+    ids=(
+        "`sort_field`와 현재 커서 파라미터가 일치하면 정렬 방향에 따른 비교 연산자를 사용한다.",
+        "`sort_field`와 일치하지 않는 커서 파라미터에는 동등 연산자를 사용한다.",
+    ),
+)
+def test_cursor_pagination_clause_expression(
+    clause: CursorPaginationClause,
+    expected: DictExpression,
+):
+    assert clause.expression() == expected
 
 
 @pytest.mark.parametrize(
