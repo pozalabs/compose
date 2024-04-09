7dd3d808506ed48b76d400220083ce9f3b049b07
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-04-09 22:02:05 +0900

    IP-4574: [compose] 기본 설정을 적용한 Logger 리턴 함수 구현 (#191)
    u: 기본 설정을 적용한 logger를 리턴하는 함수 구현

--- compose/logging.py
+++ compose/logging.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
 import logging
+import sys
+from collections.abc import Iterable
+from typing import TYPE_CHECKING, Any
 
 try:
     from loguru import logger
@@ -7,6 +12,9 @@ except ImportError:
         "The `loguru` extra must be installed to use the `compose.logging` module. "
         "Install `compose` with `loguru` extra (`compose[loguru]`)"
     )
+
+if TYPE_CHECKING:
+    from loguru import Logger
 
 
 class InterceptHandler(logging.Handler):
@@ -29,13 +37,51 @@ class InterceptHandler(logging.Handler):
         return "/health-check" not in record.getMessage()
 
 
-def intercept_logging(log_level: int) -> None:
-    """Python 내장 logging 모듈을 loguru로 대체합니다. (해당 함수를 호출하려면 `loguru`를 설치해야 합니다.)
+def intercept_logging(intercept_handler: InterceptHandler, log_level: int) -> None:
+    """Python 내장 logging 모듈을 loguru로 대체합니다. (해당 함수를 호출하려면 `loguru`를 설치해야 합니다.)"""
+    logging.basicConfig(handlers=[intercept_handler], level=log_level, force=True)
 
-    ```python
-    import compose
 
-    compose.logging.intercept_logging(logging.INFO)
-    ```
-    """
-    logging.basicConfig(handlers=[InterceptHandler()], level=log_level, force=True)
+def intercept(
+    intercept_handler: InterceptHandler,
+    log_names: Iterable[str] = (
+        "gunicorn.error",
+        "gunicorn.access",
+        "uvicorn.error",
+        "uvicorn.access",
+    ),
+) -> None:
+    intercept_logging(intercept_handler=intercept_handler, log_level=logging.INFO)
+    for name in log_names:
+        logging.getLogger(name).handlers = [intercept_handler]
+
+
+def get_default_logging_config(serialize_log: bool) -> dict[str, Any]:
+    non_serialized_format = (
+        "{time:YYYY-MM-DD HH:mm:ss.SSS} | <level>{level: <8}</level> | <level>{message}</level>"
+    )
+    return {
+        "sink": sys.stdout,
+        "diagnose": False,
+        "format": "{message}" if serialize_log else non_serialized_format,
+        "serialize": serialize_log,
+    }
+
+
+def get_default_logger(
+    log_level: int,
+    serialize_log: bool,
+    **config: Any,
+) -> Logger:
+    intercept_handler = InterceptHandler()
+    logging.basicConfig(handlers=[intercept_handler], level=log_level, force=True)
+    intercept(intercept_handler=intercept_handler)
+    logger.configure(
+        handlers=[
+            {
+                **get_default_logging_config(serialize_log),
+                **config,
+            }
+        ]
+    )
+    return logger
