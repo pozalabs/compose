2e7b10174f023c97995c9bfc2f98e6e9129f08db
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-04-10 21:43:06 +0900

    IP-4585: [compose] HTTP Basic 인증 구현 (#194)
    * u: AuthorizationError 에러 핸들러 추가
    
    * c: HTTPBasicAuth 구현
    
    * u: HTTP Basic Auth 예제 추가

--- examples/fastapi_example/src/app/exceptions.py
+++ examples/fastapi_example/src/app/exceptions.py
@@ -11,6 +11,10 @@ EXCEPTION_HANDLER_INFOS = (
         exc_cls=compose.exceptions.DomainValidationError,
         status_code=http.HTTPStatus.UNPROCESSABLE_ENTITY,
     ),
+    compose.fastapi.ExceptionHandlerInfo.for_exc(
+        exc_cls=compose.exceptions.AuthorizationError,
+        status_code=http.HTTPStatus.UNAUTHORIZED,
+    ),
     compose.fastapi.ExceptionHandlerInfo.for_request_validation_error(),
     compose.fastapi.ExceptionHandlerInfo.for_pydantic_validation_error(),
     compose.fastapi.ExceptionHandlerInfo.default(),
