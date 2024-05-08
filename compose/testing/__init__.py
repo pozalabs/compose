97bb2dceb39988643f4b44730805719fd0375473
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-05-08 23:48:15 +0900

    IP-4712: [compose] Enum 플래그 프로퍼티 테스트 생성 기능 구현 (#204)
    * c: enum 플래그 프로퍼티 테스트 생성 함수 구현
    
    * c: 테스트 작성

--- compose/testing/__init__.py
+++ compose/testing/__init__.py
@@ -3,5 +3,7 @@ try:
 except ImportError:
     raise ImportError("Install `pytest` to use testing fixtures")
 
+from .enums import *  # noqa: F401, F403
 from .fixture import *  # noqa: F401, F403
 from .hook import *  # noqa: F401, F403
+from .param import *  # noqa: F401, F403
