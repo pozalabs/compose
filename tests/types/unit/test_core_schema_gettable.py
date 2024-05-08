e8d6c5f185ad2c0c9ee1f4cf32b0bbd6ec05f8fa
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-05-09 00:15:02 +0900

    u: CoreSchemaGettable 테스트

--- tests/types/unit/test_core_schema_gettable.py
+++ tests/types/unit/test_core_schema_gettable.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import pytest
 
 import compose
@@ -7,12 +5,11 @@ import compose
 if not compose.compat.IS_PYDANTIC_V2:
     pytest.skip("pydantic v2 only", allow_module_level=True)
 else:
-    from pydantic import GetCoreSchemaHandler, TypeAdapter
+    from pydantic import TypeAdapter
     from pydantic.v1.validators import str_validator
-    from pydantic_core import core_schema
 
 
-class StrippedStr(str):
+class StrippedStr(str, compose.types.CoreSchemaGettable):
     @classmethod
     def __get_validators__(cls):
         yield str_validator
@@ -21,12 +18,6 @@ class StrippedStr(str):
     @classmethod
     def validate(cls, v: str) -> str:
         return v.strip()
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls, source_type: Any, handler: GetCoreSchemaHandler
-    ) -> core_schema.CoreSchema:
-        return compose.types.get_pydantic_core_schema(cls, handler(str))
 
 
 def test_get_pydantic_core_schema():
