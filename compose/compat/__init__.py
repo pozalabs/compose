5530e344a8b62d7da172080d6ad220f9001650a1
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-05-15 18:30:00 +0900

    u: compat 모듈을 패키지로 변경

---
+++ compose/compat/__init__.py
@@ -0,0 +1,17 @@
+from .model import (
+    model_dump,
+    model_dump_json,
+    model_schema,
+    model_validate,
+    model_validate_json,
+    validate_obj,
+)
+
+__all__ = [
+    "validate_obj",
+    "model_schema",
+    "model_validate",
+    "model_validate_json",
+    "model_dump",
+    "model_dump_json",
+]
