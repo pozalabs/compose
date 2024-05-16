91ffd528551f4854ff1991a11a72731e2e1ede78
jeehoon-poza <jeehoon@pozalabs.com>
2024-05-16 16:37:15 +0900

    c: str 베이스 타입 추가

---
+++ compose/types/vo.py
@@ -0,0 +1,21 @@
+from collections.abc import Callable, Generator
+from typing import Any, Self, TypeVar
+
+from . import CoreSchemaGettable
+from compose import compat
+
+T = TypeVar("T")
+
+
+def caster(factory: Callable[[Any], T], /) -> Callable[[Any], T]:
+    def _cast(v: Any) -> T:
+        return factory(v)
+
+    return _cast
+
+
+class Str(str, CoreSchemaGettable[str]):
+    @classmethod
+    def __get_validators__(cls) -> Generator[[Callable[[Any], Self]], None, None]:
+        yield compat.str_validator
+        yield caster(cls)
