45b5683f39aa6a89d97bf8d8876ba074340b0d07
poza-soyeon <132875256+poza-soyeon@users.noreply.github.com>
2024-05-16 15:32:00 +0900

    u: `compat.validate_obj` 활용

--- tests/compat/test_type_validator.py
+++ tests/compat/test_type_validator.py
@@ -1,14 +1,7 @@
 from collections.abc import Callable, Generator
 from typing import Any, Self
 
-import pytest
-
 import compose
-
-if compose.compat.IS_PYDANTIC_V2:
-    from pydantic import TypeAdapter
-else:
-    from pydantic import parse_obj_as
 
 
 class SomeStr(str, compose.types.CoreSchemaGettable[str]):
@@ -18,11 +11,5 @@ class SomeStr(str, compose.types.CoreSchemaGettable[str]):
         yield lambda v: cls(v)
 
 
-@pytest.mark.skipif(not compose.compat.IS_PYDANTIC_V2, reason="pydantic v2 only")
 def test_str_validator():
-    assert TypeAdapter(SomeStr).validate_python("test") == SomeStr("test")
-
-
-@pytest.mark.skipif(compose.compat.IS_PYDANTIC_V2, reason="pydantic v1 only")
-def test_str_validator_v1():
-    assert parse_obj_as(SomeStr, "test") == SomeStr("test")
+    assert compose.compat.validate_obj(SomeStr, "test") == SomeStr("test")
