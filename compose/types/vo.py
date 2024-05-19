4e4896188c85ddf84cdd0cb2b942aff05aba7673
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-05-19 16:45:42 +0900

    c: 제네릭 리스트 생성 함수 구현

--- compose/types/vo.py
+++ compose/types/vo.py
@@ -1,9 +1,10 @@
+import types
 from collections.abc import Callable, Generator
-from typing import Any, Self, TypeVar
+from typing import Any, Self, TypeVar, cast
 
 from compose import compat
 
-from . import CoreSchemaGettable
+from .helper import CoreSchemaGettable
 
 T = TypeVar("T")
 
@@ -48,3 +49,41 @@ class IntList(list[int], CoreSchemaGettable[list[int]]):
     def __get_validators__(cls) -> Generator[[Callable[[Any], Self]], None, None]:
         yield compat.list_validator
         yield caster(cls)
+
+
+def _create_list_type(t: T, /) -> type[list[T]]:
+    def __get_validators__(c):
+        yield compat.list_validator
+        yield caster(c)
+
+    return cast(
+        type[list[T]],
+        types.new_class(
+            f"{t.__name__.title()}List",
+            (list[t], CoreSchemaGettable[list[t]]),
+            exec_body=lambda ns: ns.update(
+                {
+                    "__get_validators__": classmethod(__get_validators__),
+                }
+            ),
+        ),
+    )
+
+
+def create_list_type() -> Callable[[[T]], type[list[T]]]:
+    cache = {}
+
+    def factory(t: T) -> type[list[T]]:
+        type_name = t.__name__
+
+        if (cached := cache.get(type_name)) is not None:
+            return cached
+
+        _result = _create_list_type(t)
+        cache[type_name] = _result
+        return _result
+
+    return factory
+
+
+TypedList = create_list_type()
