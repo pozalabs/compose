511b4c6b90fa4585c8f293f077eed3df2ac5f331
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-05-20 13:59:02 +0900

    u: ValidatorGenerator 타입 오기 수정

--- compose/typing.py
+++ compose/typing.py
@@ -2,4 +2,4 @@ from collections.abc import Callable, Generator
 from typing import Any, Self, TypeAlias
 
 Validator: TypeAlias = Callable[[Any], Self]
-ValidatorGenerator: TypeAlias = Generator[[Validator], None, None]
+ValidatorGenerator: TypeAlias = Generator[Validator, None, None]
