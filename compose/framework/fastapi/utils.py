afd67fc4faac32448d11b95047208604fff51c36
poza-soyeon <132875256+poza-soyeon@users.noreply.github.com>
2024-05-20 17:12:49 +0900

    u: sentry init 설정 모듈 추가

--- compose/framework/fastapi/utils.py
+++ compose/framework/fastapi/utils.py
@@ -1,10 +1,77 @@
+import enum
 import functools
 from collections.abc import Awaitable
+from typing import Any, Callable, Self, TypeAlias
 
 import sentry_sdk
 from fastapi import Request, Response
+from sentry_sdk.integrations import Integration
 
-from compose import fastapi
+from compose import BaseModel, fastapi
+
+
+class Level(enum.StrEnum):
+    INFO = enum.auto()
+    WARNING = enum.auto()
+    ERROR = enum.auto()
+
+
+class ErrorEvent(BaseModel):
+    level: Level
+
+    @classmethod
+    def for_info(cls, level: Level) -> Self:
+        return cls(level=Level.INFO)
+
+    @classmethod
+    def for_warning(cls) -> Self:
+        return cls(level=Level.WARNING)
+
+    @classmethod
+    def for_error(cls) -> Self:
+        return cls(level=Level.ERROR)
+
+
+SentryHook: TypeAlias = Callable[[dict[str, Any], dict[str, Any]], dict[str, Any]]
+
+
+def create_before_send_hook(
+    error: dict[str, ErrorEvent], default_error_level: Level = Level.WARNING
+) -> SentryHook:
+    def before_send(event: dict[str, Any], hint: dict[str, Any]) -> dict[str, Any]:
+        exc_name = ""
+        if "exc_info" in hint:
+            exc_type, *_ = hint["exc_info"]
+            exc_name = exc_type.__name__
+
+        error_event = error.get(exc_name)
+        event["level"] = default_error_level if error_event is None else error_event.level
+        return event
+
+    return before_send
+
+
+def init_sentry(
+    dsn: str | None,
+    integrations: list[Integration],
+    environment: str,
+    tags: dict[str, str],
+    before_send: SentryHook | None = None,
+    **kwargs,
+) -> None:
+    if dsn is None:
+        return
+
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=integrations,
+        environment=environment,
+        before_send=before_send,
+        **kwargs,
+    )
+    scope = sentry_sdk.Scope.get_current_scope()
+    for key, value in tags.items():
+        scope.set_tag(key, value)
 
 
 def capture_error(handler: fastapi.ExceptionHandler) -> fastapi.ExceptionHandler:
