98f40fa51fcd43f5abfa9b3b0de446558e09e4e4
jeehoon-poza <118701416+jeehoon-poza@users.noreply.github.com>
2024-05-30 13:55:05 +0900

    IP-4837: [compose] 여러 DB를 사용하는 경우의 인덱스 생성 지원 (#227)
    * u: 데이터베이스별 컬렉션의 인덱스를 설정할 수 있도록 수정
    
    * u: 동일한 컬렉션명이 있을 경우 에러 일으키도록 수정
    
    * u: 컬렉션을 새로 추가하는 경우, 팩토리 메서드에서 인덱스를 생성하도록 수정

--- compose/repository/mongo.py
+++ compose/repository/mongo.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import enum
 import inspect
+from collections.abc import Iterable
 from typing import Any, ClassVar, Generic, TypeVar, get_args, get_type_hints
 
 import pendulum
@@ -61,6 +62,8 @@ class MongoRepository(base.BaseRepository, Generic[EntityType]):
         cls._validate_session_requirement(
             kwargs.get("session_requirement", SessionRequirement.OPTIONAL)
         )
+        if cls.__collection_name__ in registry:
+            raise ValueError(f"Collection name {cls.__collection_name__} is already in use")
         registry[cls.__collection_name__] = cls.__indexes__
 
     @classmethod
@@ -91,6 +94,11 @@ class MongoRepository(base.BaseRepository, Generic[EntityType]):
     @classmethod
     def create(cls, database: Database, **kwargs) -> MongoRepository:
         collection = database.get_collection(cls.__collection_name__, **kwargs)
+        if (
+            cls.__collection_name__ not in database.list_collection_names()
+            and cls.__indexes__ is not None
+        ):
+            collection.create_indexes(cls.__indexes__)
 
         return cls(collection=collection)
 
@@ -205,20 +213,26 @@ class MongoRepository(base.BaseRepository, Generic[EntityType]):
         )
 
 
-def setup_indexes(mongo_uri: str, db_name: str) -> None:
+def setup_indexes(mongo_uri: str, db_names: Iterable[str]) -> None:
+    # 동일한 이름의 컬렉션이 여러 DB에 존재할 경우, 컬렉션별로 인덱스를 생성할 수 없음
     with pymongo.MongoClient(mongo_uri) as client:
-        for collection_name, indexes in registry.items():
-            if not collection_name:
-                continue
+        for db_name in db_names:
             database = client.get_database(db_name)
-            collection = database.get_collection(collection_name)
+            collection_names = database.list_collection_names()
+            for collection_name, indexes in registry.items():
+                if not collection_name or collection_name not in collection_names:
+                    continue
+                collection = database.get_collection(collection_name)
 
-            previous_index_names = {index["name"] for index in collection.list_indexes()}
-            current_indexes = indexes or []
-            current_index_names = {*(index.document["name"] for index in current_indexes), "_id_"}
+                previous_index_names = {index["name"] for index in collection.list_indexes()}
+                current_indexes = indexes or []
+                current_index_names = {
+                    *(index.document["name"] for index in current_indexes),
+                    "_id_",
+                }
 
-            for index_name in previous_index_names - current_index_names:
-                collection.drop_index(index_name)
+                for index_name in previous_index_names - current_index_names:
+                    collection.drop_index(index_name)
 
-            if indexes is not None:
-                collection.create_indexes(indexes)
+                if indexes is not None:
+                    collection.create_indexes(indexes)
