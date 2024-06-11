8aee6aeb7bc792e686c275ef03e0b2a9400619b3
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-06-12 01:21:51 +0900

    u: pydantic.v1.env_settings 관련 주석 추가

--- examples/fastapi_example/src/settings.py
+++ examples/fastapi_example/src/settings.py
@@ -1,3 +1,5 @@
+# 예제는 compose 개발 의존성을 그대로 사용하기 때문에 불가피하게 `pydantic.v1.env_settings`를 사용하였습니다.
+# `compose`를 사용하는 어플리케이션에서는 `pydantic_settings`를 사용하세요.
 from pydantic.v1.env_settings import BaseSettings, Extra
 
 # v2
