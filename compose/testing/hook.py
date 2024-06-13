1ac3395bc7b3fe15c8ce850ad1b554bf46ca446a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-06-13 18:38:24 +0900

    IP-4973: [compose] pytest 기본 플러그인 구현 (#241)
    * c: 기본 플러그인 구현
    
    * u: PytestUnknownMarkWarning 무시

--- compose/testing/hook.py
+++ compose/testing/hook.py
@@ -1,20 +1,49 @@
+import functools
+import warnings
 from collections.abc import Callable
 from pathlib import Path
 
 import pytest
 
+from compose.enums import AppEnv
+
+from .enums import TestTypeMarker
+
+
+@functools.lru_cache(1)
+def get_marker_name_to_marker() -> dict[str, pytest.MarkDecorator]:
+    return {
+        marker_name: getattr(pytest.mark, marker_name) for marker_name in TestTypeMarker.names()
+    }
+
+
+def check_env_is_allowed(env: AppEnv) -> None:
+    if env is None or env != AppEnv.TEST:
+        raise RuntimeError(f"`APP_ENV` must be set to `{AppEnv.TEST}` to run tests")
+
+
+def register_test_type_markers(config: pytest.Config) -> None:
+    for member in TestTypeMarker:
+        name, description = member.value
+        config.addinivalue_line("markers", f"{name}: {description}")
+
 
 def default_marker_getter(item: pytest.Function) -> pytest.MarkDecorator:
-    test_type_to_marker = {
-        marker.name: marker
-        for marker in [pytest.mark.unit, pytest.mark.integration, pytest.mark.e2e]
-    }
-    test_types = set(test_type_to_marker.keys())
-    default_test_type = pytest.mark.unit.name
+    with warnings.catch_warnings(
+        action="ignore",
+        category=pytest.PytestUnknownMarkWarning,
+    ):
+        marker_name_to_marker = get_marker_name_to_marker()
+        test_types = set(marker_name_to_marker.keys())
+        default_test_type = pytest.mark.unit.name
 
-    node_path = item.nodeid.split("::")[0]
-    parts = Path(node_path).parts
-    return next((part for part in parts if part in test_types), default_test_type)
+        node_path = item.nodeid.split("::")[0]
+        parts = Path(node_path).parts
+
+        mark_name = next((part for part in parts if part in test_types), default_test_type)
+        result = getattr(pytest.mark, mark_name)
+
+    return result
 
 
 def add_test_type_markers(
