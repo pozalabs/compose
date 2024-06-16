b1fff3386d07cf8186b21cab165ad36324e6b120
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-06-16 23:00:14 +0900

    u: caplog fixture 참고 문서 첨부

--- tests/opentelemetry/instrumentation/unit/test_loguru_instrumentor.py
+++ tests/opentelemetry/instrumentation/unit/test_loguru_instrumentor.py
@@ -12,6 +12,12 @@ import compose
 
 @pytest.fixture
 def caplog(caplog: LogCaptureFixture) -> Iterator[LogCaptureFixture]:
+    """
+
+    Reference:
+        https://loguru.readthedocs.io/en/stable/resources/migration.html#replacing-caplog-fixture-from-pytest-library
+    """
+
     handler_id = logger.add(
         caplog.handler,
         format="{message}",
