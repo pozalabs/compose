4fc981f8450ae51f85c2ed7dc4e8b396b8e74064
jeehoon-poza <jeehoon@pozalabs.com>
2024-06-17 12:02:41 +0900

    c: 사용자 리소스 정보를 담는 `UserResource` 클래스 구현

--- compose/auth/vo.py
+++ compose/auth/vo.py
@@ -4,3 +4,8 @@ from .. import container
 class AuthorizationGrant(container.BaseModel):
     access_token: str
     refresh_token: str | None = None
+
+
+class UserResource(container.BaseModel):
+    email: str
+    name: str
