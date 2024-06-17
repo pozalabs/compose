20039461337610342e1e37960613142bdb4dc98f
jeehoon-poza <jeehoon@pozalabs.com>
2024-06-17 15:12:31 +0900

    u: 참조 경로 업데이트

--- compose/auth/__init__.py
+++ compose/auth/__init__.py
@@ -1,5 +1,12 @@
 from .authorization_server import AuthorizationServer
 from .resource_server import ResourceServer
+from .token_decoder import JWTDecoder
 from .vo import AuthorizationGrant, UserResource
 
-__all__ = ["AuthorizationServer", "AuthorizationGrant", "ResourceServer", "UserResource"]
+__all__ = [
+    "AuthorizationServer",
+    "AuthorizationGrant",
+    "ResourceServer",
+    "UserResource",
+    "JWTDecoder",
+]
