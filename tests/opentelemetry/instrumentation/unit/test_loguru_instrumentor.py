cb311083f818e484ec05d07886c26d016668c331
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-06-18 13:15:34 +0900

    u: lock 파일 업데이트

---
+++ tests/opentelemetry/instrumentation/unit/test_loguru_instrumentor.py
@@ -0,0 +1,68 @@
+import logging
+from collections.abc import Iterator
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from loguru import logger
+from opentelemetry import trace
+from opentelemetry.test.test_base import TestBase
+
+import compose
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture) -> Iterator[LogCaptureFixture]:
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,
+    )
+    yield caplog
+    logger.remove(handler_id)
+
+
+class TestLoguruInstrumentor(TestBase):
+    @pytest.fixture(autouse=True)
+    def inject_fixtures(self, caplog: LogCaptureFixture):
+        self.caplog = caplog
+
+    def setUp(self):
+        super().setUp()
+        self.instrumentor = compose.opentelemetry.LoguruInstrumentor()
+        self.instrumentor.instrument()
+        self.tracer = trace.get_tracer(__name__)
+
+    def tearDown(self):
+        super().tearDown()
+        self.instrumentor.uninstrument()
+
+    def test_trace_context_injection(self):
+        with self.tracer.start_as_current_span("span1") as span:
+            with self.caplog.at_level(logging.INFO):
+                ctx = span.get_span_context()
+
+                logger.info("Hello")
+                record = self.caplog.records[0]
+                extra = record.extra
+
+                expected_trace_context = {
+                    "otel_service_name": "unknown_service",
+                    "otel_span_id": trace.format_span_id(ctx.span_id),
+                    "otel_trace_id": trace.format_trace_id(ctx.trace_id),
+                    "otel_trace_sampled": ctx.trace_flags.sampled,
+                }
+
+                assert extra == expected_trace_context
+
+    def test_uninstrument(self):
+        self.instrumentor.uninstrument()
+        with self.tracer.start_as_current_span("span1"):
+            with self.caplog.at_level(logging.INFO):
+                logger.info("Hello")
+
+                record = self.caplog.records[0]
+                extra = record.extra
+
+                assert not extra
