fa20622f6c8cd787d821fe35d296327d453ca84f
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-07-09 23:49:40 +0900

    IP-5127: [compose] DateTime 상속시 추가 검증 로직이 적용되지 않는 오류 수정 (#262)
    * u: DateTime 검증을 확장할 수 있게 변경
    
    * u: __get_pydantic_core_schema__ 간소화

--- compose/types/datetime.py
+++ compose/types/datetime.py
@@ -7,10 +7,11 @@ from typing import Any
 import pendulum
 
 from .. import compat
+from .helper import get_pydantic_core_schema
 
 if compat.IS_PYDANTIC_V2:
     from pydantic import GetCoreSchemaHandler
-    from pydantic_core import CoreSchema, core_schema
+    from pydantic_core import CoreSchema
 else:
     from pydantic.datetime_parse import parse_datetime
 
@@ -18,24 +19,20 @@ else:
 class DateTime(pendulum.DateTime):
     """https://stackoverflow.com/a/76719893"""
 
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable[[Any], pendulum.DateTime], None, None]:
+        if not compat.IS_PYDANTIC_V2:
+            yield parse_datetime
+        yield cls._instance
+
+    @classmethod
+    def _instance(cls, v: datetime.datetime | pendulum.DateTime) -> pendulum.DateTime:
+        return pendulum.instance(obj=v, tz=pendulum.UTC)
+
     if compat.IS_PYDANTIC_V2:
 
         @classmethod
         def __get_pydantic_core_schema__(
             cls, source_type: Any, handler: GetCoreSchemaHandler
         ) -> CoreSchema:
-            return core_schema.no_info_after_validator_function(
-                cls._instance,
-                handler(datetime.datetime),
-            )
-
-    else:
-
-        @classmethod
-        def __get_validators__(cls) -> Generator[Callable[[Any], pendulum.DateTime], None, None]:
-            yield parse_datetime
-            yield cls._instance
-
-    @classmethod
-    def _instance(cls, v: datetime.datetime | pendulum.DateTime) -> pendulum.DateTime:
-        return pendulum.instance(obj=v, tz=pendulum.UTC)
+            return get_pydantic_core_schema(cls, handler(datetime.datetime))
