581c205fb527d3bd90ebc4bdbb12ddd93153d669
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-07-13 23:03:52 +0900

    IP-5213: [compose] 로컬 메시지 처리 미들웨어 구현 (#265)
    * u: consumer 모듈을 패키지로 변경
    
    * u: queue 하위 모듈 노출 방법 변경
    
    * c: FastAPI 메시지 처리 미들웨어 구현
    
    * u: DEFAULT_HOOKS 선언 위치 변경
    
    * u: hook 메시지 직렬화 오류 수정
    
    * c: MessageConsumerASGIMiddleware 테스트 작성

--- compose/messaging/consumer/general.py
+++ compose/messaging/consumer/general.py
@@ -1,47 +1,13 @@
 import asyncio
-import json
 import logging
-from collections.abc import Callable
-from typing import Literal, TypeAlias
 
-from . import model
-from .messagebus import MessageBus
-from .queue import MessageQueue
-from .signal_handler import DefaultSignalHandler, SignalHandler
-
-HookEventType = Literal[
-    "on_start",
-    "on_receive",
-    "on_receive_error",
-    "on_consume",
-    "on_consume_error",
-    "on_shutdown",
-]
-HookArgType: TypeAlias = str | model.EventMessage | Exception
-Hook: TypeAlias = Callable[[HookArgType], None]
+from .. import model
+from ..messagebus import MessageBus
+from ..queue.base import MessageQueue
+from ..signal_handler import DefaultSignalHandler, SignalHandler
+from .hook import DEFAULT_HOOKS, Hook, HookArgType, HookEventType, default_hook
 
 logger = logging.getLogger("compose")
-
-
-def log_event_message(log_message: str, message: model.EventMessage) -> None:
-    logger.info(
-        f"{log_message}: {json.dumps(message.encode(), ensure_ascii=False)}",
-        extra={"event_message": message.encode()},
-    )
-
-
-def log_exception(log_message: str, exc: Exception) -> None:
-    logger.exception(log_message, exc_info=exc, stack_info=True)
-
-
-DEFAULT_HOOKS = {
-    "on_start": [logger.info],
-    "on_receive": [lambda message: log_event_message("Received message", message)],
-    "on_receive_error": [lambda exc: log_exception("Failed to receive message", exc)],
-    "on_consume": [lambda message: log_event_message("Consumed message", message)],
-    "on_consume_error": [lambda exc: log_exception(f"Failed to consume message due to {exc}", exc)],
-    "on_shutdown": [logger.info],
-}
 
 
 class MessageConsumer:
@@ -57,7 +23,7 @@ class MessageConsumer:
         self.hooks = DEFAULT_HOOKS | (hooks or {})
         self.signal_handler = signal_handler or DefaultSignalHandler()
 
-        self._default_hook = lambda _: None
+        self._default_hook = default_hook
 
     async def run(self) -> None:
         self._execute_hook("on_start", "MessageConsumer started")
--- compose/messaging/queue/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import MessageQueue
-from .local import LocalMessageQueue, event_store
-from .sqs import SqsMessageQueue
-
-__all__ = ["MessageQueue", "SqsMessageQueue", "LocalMessageQueue", "event_store"]
