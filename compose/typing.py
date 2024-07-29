ba78d559f35babcfd05f1d7fcf18e5a43d1b4e42
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-07-30 00:39:22 +0900

    c: IdFactory 제네릭 타입 구현

--- compose/typing.py
+++ compose/typing.py
@@ -1,5 +1,9 @@
 from collections.abc import Callable, Generator
-from typing import Any, Self, TypeAlias
+from typing import Any, Self, TypeAlias, TypeVar
+
+T = TypeVar("T")
 
 Validator: TypeAlias = Callable[[Any], Self]
 ValidatorGenerator: TypeAlias = Generator[Validator, None, None]
+
+IdFactory: TypeAlias = Callable[..., T]
