e5944e0fdabb5c926ccb7c0d4b9cc88afb769a27
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-08-16 18:02:20 +0900

    d: 불필요한 테스트 제거

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -12,11 +12,6 @@ class SchemaModel(compose.schema.Schema):
     name: str
 
 
-def test_model_validate():
-    model = Model(name="test")
-    assert SchemaModel.model_validate(model.dict()) == SchemaModel(name="test")
-
-
 def test_validate_on_copy():
     model = Model(name="test")
 
