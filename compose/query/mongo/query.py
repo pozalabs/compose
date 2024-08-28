1b4de16fcb65d39b65d0e429d93a43f01f94e6ac
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-08-28 14:51:37 +0900

    IP-5502: [compose] MongoRepository 쿼리 조작 개선 (#293)
    * c: Find 구현
    
    * u: find, list 메서드 구현
    
    * d: Find 제거
    
    * u: find, list 메서드의 filter 외 인자를 kw only로 변경

--- compose/query/mongo/query.py
+++ compose/query/mongo/query.py
@@ -1,16 +1,16 @@
 import abc
 from typing import Any
 
-from .. import base
+from ..base import OffsetPaginationQuery, Query
 
 
-class MongoQuery(base.Query, abc.ABC):
+class MongoQuery(Query, abc.ABC):
     @abc.abstractmethod
     def to_query(self) -> list[dict[str, Any]]:
         raise NotImplementedError
 
 
-class MongoFilterQuery(base.OffsetPaginationQuery, MongoQuery):
+class MongoFilterQuery(OffsetPaginationQuery, MongoQuery):
     @abc.abstractmethod
     def to_query(self) -> list[dict[str, Any]]:
         raise NotImplementedError
