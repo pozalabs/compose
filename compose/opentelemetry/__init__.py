c5a050e245072cc30cf327a18bc3ee2504d8b181
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-09-02 17:50:47 +0900

    IP-5542: [compose] OpenTelemetry 계측 보일러 플레이트 제공 (#295)
    c: 기본 TracerProvider 초기화 함수 구현

--- compose/opentelemetry/__init__.py
+++ compose/opentelemetry/__init__.py
@@ -4,5 +4,6 @@ except ImportError:
     raise ImportError("Install `opentelemetry` extra to use opentelemetry features")
 
 from .instrumentation.loguru.instrumentor import LoguruInstrumentor
+from .tracer_provider import get_default_tracer_provider
 
-__all__ = ["LoguruInstrumentor"]
+__all__ = ["LoguruInstrumentor", "get_default_tracer_provider"]
