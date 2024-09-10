0499fd838d80f3f833fb71e0e2051e3ddfa519e5
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-09-10 13:44:09 +0900

    IP-5587: [compose] 기본 TracerProvider 초기화 시의 유연성 확대 (#303)
    u: TracerProvider 초기화 인자 추가 허용

--- compose/opentelemetry/tracer_provider.py
+++ compose/opentelemetry/tracer_provider.py
@@ -1,7 +1,14 @@
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Attributes, LabelValue, Resource
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace import (
+    ConcurrentMultiSpanProcessor,
+    IdGenerator,
+    SpanLimits,
+    SynchronousMultiSpanProcessor,
+    TracerProvider,
+)
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.sampling import Sampler
 
 
 class ServiceResourceAttrs:
@@ -26,8 +33,22 @@ class ServiceResourceAttrs:
 def get_default_tracer_provider(
     resource_attrs: ServiceResourceAttrs,
     exporter_endpoint: str,
+    sampler: Sampler | None = None,
+    shutdown_on_exit: bool = True,
+    active_span_processor: (
+        SynchronousMultiSpanProcessor | ConcurrentMultiSpanProcessor | None
+    ) = None,
+    id_generator: IdGenerator | None = None,
+    span_limits: SpanLimits | None = None,
 ) -> TracerProvider:
-    tracer_provider = TracerProvider(resource=Resource.create(resource_attrs.to_attrs()))
+    tracer_provider = TracerProvider(
+        resource=Resource.create(resource_attrs.to_attrs()),
+        sampler=sampler,
+        shutdown_on_exit=shutdown_on_exit,
+        active_span_processor=active_span_processor,
+        id_generator=id_generator,
+        span_limits=span_limits,
+    )
     exporter = OTLPSpanExporter(endpoint=exporter_endpoint)
     tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
     return tracer_provider
