629fd285c4d467edbc2f70352a35e8f760f60b2a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-10-18 10:44:33 +0900

    u: execute_in_pool 타입 힌트 변경

--- compose/concurrent.py
+++ compose/concurrent.py
@@ -1,13 +1,9 @@
 import concurrent.futures
 import functools
 from collections.abc import Callable
-from typing import TypeVar
-
-K = TypeVar("K")
-T = TypeVar("T")
 
 
-def execute_in_pool(
+def execute_in_pool[K, T](
     pool_factory: Callable[[], concurrent.futures.Executor],
     funcs: dict[K, functools.partial[T]],
     timeout: int | None = None,
