c2650dddf9308f34f6b3c7c644cf9ca4792fd0ae
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-10-18 17:59:01 +0900

    c: 유닛 테스트 작성

---
+++ tests/test_day_period.py
@@ -0,0 +1,31 @@
+import pendulum
+import pytest
+
+import compose
+
+
+@pytest.mark.parametrize(
+    "dt, expected",
+    [
+        (
+            pendulum.datetime(2024, 10, 10, 10, 10),
+            compose.DayPeriod(
+                start=pendulum.datetime(2024, 10, 10),
+                end=pendulum.datetime(2024, 10, 11),
+            ),
+        ),
+        (
+            pendulum.datetime(2024, 10, 10, 8, tz="Asia/Seoul"),
+            compose.DayPeriod(
+                start=pendulum.datetime(2024, 10, 9, 15),
+                end=pendulum.datetime(2024, 10, 10, 15),
+            ),
+        ),
+    ],
+    ids=(
+        "UTC 날짜를 입력하는 경우 UTC 기준 시작 시간을 시작 시간으로 사용한다.",
+        "KST 날짜를 입력하는 경우 KST 기준 시작 시간을 UTC로 변환한 시각을 시작 시간으로 사용한다.",
+    ),
+)
+def test_for_day(dt: pendulum.DateTime, expected: compose.DayPeriod):
+    assert compose.DayPeriod.for_day(dt) == expected
