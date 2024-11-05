bddee6bf4f166cb1a869999cf6dce47732909f2a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-11-05 18:36:46 +0900

    d: ORJSONSchema 제거

--- compose/schema/__init__.py
+++ compose/schema/__init__.py
@@ -10,10 +10,3 @@ __all__ = [
     "schema_by_field_name",
     "schema_excludes",
 ]
-
-try:
-    from .orjson_schema import ORJSONSchema  # noqa: F401
-
-    __all__.append("ORJSONSchema")
-except ImportError:
-    pass
