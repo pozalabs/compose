6e7f64d0c9b1036fc0ace87cda886a3b86f6d534
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-11-26 19:21:36 +0900

    u: RUF100 규칙 무시 관련 주석 추가

--- compose/__init__.py
+++ compose/__init__.py
@@ -72,6 +72,8 @@ except ImportError:
     pass
 
 try:
+    # TODO: `fastapi` 패키지에서 `import fastapi`를 사용하고 있어 발생하는 RUF100 규칙 위배 오류 해결
+    # TODO: `import xxx` 대신 `importlib.util.find_spec()`을 사용하여 패키지 존재 여부 확인
     from . import fastapi  # noqa: F401, RUF100
 
     __all__.append("fastapi")
