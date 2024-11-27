afc236f1229f64f288ed6a876fba60009c5d0b4b
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-11-27 19:18:40 +0900

    u: httpx 패키지 설치 여부 판단 로직 변경

--- compose/httpx/__init__.py
+++ compose/httpx/__init__.py
@@ -1,6 +1,6 @@
-try:
-    import httpx  # noqa: F401
-except ImportError:
+from .._internal import is_package_installed
+
+if not is_package_installed("httpx"):
     raise ImportError("Install `httpx` extra to use httpx features")
 
 from .auth.api_key import HeaderAPIKeyAuth
