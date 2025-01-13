18d0276d0b2446be706cc6d873283466a416350a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-01-13 18:39:53 +0900

    u: MongoDbContainer 노출

--- compose/testcontainers/__init__.py
+++ compose/testcontainers/__init__.py
@@ -2,3 +2,7 @@ from .._internal import is_package_installed
 
 if not is_package_installed("testcontainers"):
     raise ImportError("Install `testcontainers` to use testing fixtures")
+
+from .mongodb import MongoDbContainer
+
+__all__ = ["MongoDbContainer"]
