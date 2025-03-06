6ff2b2c17c8f3373ee0db9b2b65582f79d82130c
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-06 16:13:57 +0900

    d: localstack 제거

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,3 @@
-from collections.abc import Generator
-
-import pytest
-from testcontainers.localstack import LocalStackContainer
-
 pytest_plugins = [
     "compose.testing.plugin.test_type_marker",
 ]
-
-
-@pytest.fixture(scope="session", autouse=True)
-def start_localstack() -> Generator[LocalStackContainer, None, None]:
-    localstack = LocalStackContainer(
-        "localstack/localstack:2.0.2",
-        region_name="ap-northeast-2",
-    ).with_services("sqs")
-
-    localstack.start()
-
-    yield localstack
-    localstack.stop()
