4198714ac3da674feeafa032ae672baee6d74c55
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-06 12:00:53 +0900

    c: ThreadPoolExecutor 구현

--- compose/concurrent.py
+++ compose/concurrent.py
@@ -1,6 +1,9 @@
 import concurrent.futures
 import functools
-from collections.abc import Callable
+from collections.abc import Callable, Hashable
+from typing import Literal, overload
+
+from pydantic.dataclasses import dataclass
 
 
 def execute_in_pool[K, T](
@@ -19,3 +22,71 @@ def execute_in_pool[K, T](
             result[future_to_key[future]] = future.result()
 
     return result
+
+
+class ThreadPoolJob[K: Hashable, **P, T]:
+    def __init__(
+        self,
+        key: K,
+        func: Callable[P, T],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ):
+        self.key = key
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+
+@dataclass
+class Result[K: Hashable, T]:
+    key: K
+    value: T
+
+
+class ThreadPoolExecutor:
+    def __init__(self, max_workers: int, timeout: int | None = None):
+        self.max_workers = max_workers
+        self.timeout = timeout
+
+    @overload
+    def execute[K, **P, T](
+        self,
+        jobs: list[ThreadPoolJob[K, P, T]],
+        group: Literal[True] = True,
+        max_workers: int | None = None,
+        timeout: int | None = None,
+    ) -> dict[K, Result[K, T]]: ...
+
+    @overload
+    def execute[K, **P, T](
+        self,
+        jobs: list[ThreadPoolJob[K, P, T]],
+        group: Literal[False] = False,
+        max_workers: int | None = None,
+        timeout: int | None = None,
+    ) -> list[Result[K, T]]: ...
+
+    def execute[K, **P, T](
+        self,
+        jobs: list[ThreadPoolJob[K, P, T]],
+        group: bool = False,
+        max_workers: int | None = None,
+        timeout: int | None = None,
+    ) -> list[Result[K, T]] | dict[K, Result[K, T]]:
+        result = []
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=max_workers or self.max_workers
+        ) as executor:
+            future_to_key = {}
+
+            for job in jobs:
+                future = executor.submit(job.func, *job.args, **job.kwargs)
+                future_to_key[future] = job.key
+
+            for future in concurrent.futures.as_completed(
+                future_to_key, timeout=timeout or self.timeout
+            ):
+                result.append(Result(key=future_to_key[future], value=future.result()))
+
+        return {result.key: result for result in result} if group else result
