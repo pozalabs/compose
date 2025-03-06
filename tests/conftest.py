5538755e5102be7b2c388bdf9cc6d9474d63d614
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-06 18:31:49 +0900

    IP-6266: [compose] 데코레이터를 통한 MongoDB 트랜잭션 사용 기능 구현 (실험적) (#356)
    * c: mongo_transactional 구현
    
    * c: mongo_transactional 테스트 작성
    
    * u: localstack 활성화
    
    * d: localstack 제거
    
    * u: MongoDbContainer 사용 방법 변ㄴ경
    
    * u: 테스트 수정
    
    * u: mongo container를 글로벌로 선언
    
    * u: 테스트 수정
    
    * u: 테스트 수정
    
    * u: autouse 활성화
    
    * u: autouse 제거
    
    * d: uow 테스트 제거

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,30 @@
+import os
+
+import pymongo
+import pytest
+
+import compose
+
 pytest_plugins = [
     "compose.testing.plugin.test_type_marker",
 ]
+
+
+mongodb = compose.testcontainers.MongoDbContainer("mongo:8.0")
+
+
+@pytest.fixture(scope="session")
+def mongodb_container(request: pytest.FixtureRequest):
+    mongodb.start()
+    request.addfinalizer(mongodb.stop)
+
+    os.environ["MONGO_URI"] = mongodb.get_connection_url()
+    os.environ["MONGO_USERNAME"] = mongodb.username
+    os.environ["MONGO_PASSWORD"] = mongodb.password
+
+    yield mongodb
+
+
+@pytest.fixture
+def mongo_client(mongodb_container: compose.testcontainers.MongoDbContainer) -> pymongo.MongoClient:
+    return pymongo.MongoClient(mongodb_container.get_connection_url())
