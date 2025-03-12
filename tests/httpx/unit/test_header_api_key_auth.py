de8a7b8501dee4404e88ae317f2e95bbee0745f5
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-12 17:42:14 +0900

    Revert "IP-6287: [compose] httpx 다중 키 헤더 인증 구현 (#369)"
    This reverts commit d88c2b625f721ee38c2a55a91816a28f22b972ac.

--- tests/httpx/unit/test_header_api_key_auth.py
+++ tests/httpx/unit/test_header_api_key_auth.py
@@ -1,59 +1,20 @@
 import http
 
 import pytest
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 from fastapi.params import Depends
-from fastapi.security.api_key import APIKeyBase
 from starlette.testclient import TestClient
 
 import compose
 
 
-class HeaderAuth(APIKeyBase):
-    def __init__(
-        self,
-        *,
-        secrets: dict[str, str],
-        auto_error: bool = True,
-        scheme_name: str | None = None,
-        description: str | None = None,
-    ):
-        self.secrets = secrets
-        self.auto_error = auto_error
-        self.scheme_name = scheme_name
-        self.description = description
-
-    async def __call__(self, request: Request) -> dict[str, str] | None:
-        exc = HTTPException(
-            status_code=http.HTTPStatus.UNAUTHORIZED,
-            detail="Not authenticated",
-        )
-
-        secrets = {name: value for name, value in request.headers.items() if name in self.secrets}
-
-        if secrets != self.secrets:
-            if self.auto_error:
-                raise exc
-            else:
-                return None
-
-        return secrets
-
-
 @pytest.fixture
 def app() -> FastAPI:
     _app = FastAPI()
-    header_single_key_auth = HeaderAuth(secrets={"x-api-key": "api-key"})
-    header_multi_keys_auth = HeaderAuth(
-        secrets={"x-client-id": "client-id", "x-client-secret": "client-secret"}
-    )
+    api_key_header = compose.fastapi.APIKeyHeader(api_key="api-key")
 
-    @_app.get("/items/single-key-auth", dependencies=[Depends(header_single_key_auth)])
-    def list_items_single_key_auth():
-        return [{"name": "item_1"}, {"name": "item_2"}]
-
-    @_app.get("/items/multi-keys-auth", dependencies=[Depends(header_multi_keys_auth)])
-    def list_items_multi_keys_auth():
+    @_app.get("/items", dependencies=[Depends(api_key_header)])
+    def create_item():
         return [{"name": "item_1"}, {"name": "item_2"}]
 
     return _app
@@ -65,45 +26,34 @@ def client(app: FastAPI) -> TestClient:
 
 
 @pytest.mark.parametrize(
-    "endpoint, auth, expected_status_code",
+    "auth, expected_status_code",
     [
         (
-            "/items/single-key-auth",
-            compose.httpx.HeaderAPIKeyAuth.single("api-key"),
+            compose.httpx.HeaderAPIKeyAuth(api_key="api-key"),
             http.HTTPStatus.OK,
         ),
         (
-            "/items/single-key-auth",
-            compose.httpx.HeaderAPIKeyAuth.single("invali-api-key"),
+            compose.httpx.HeaderAPIKeyAuth(api_key="invali-api-key"),
             http.HTTPStatus.UNAUTHORIZED,
-        ),
-        (
-            "/items/multi-keys-auth",
-            compose.httpx.HeaderAPIKeyAuth(
-                {"x-client-id": "client-id", "x-client-secret": "client-secret"}
-            ),
-            http.HTTPStatus.OK,
         ),
     ],
     ids=(
         "올바른 API Key를 사용한 인증",
-        "올바르지 않은 API Key를 사용한 인증",
-        "올바른 Multi Keys를 사용한 인증",
+        "올바르지 않은 API Key한 인증",
     ),
 )
-def test_single_key_auth(
+def test_header_api_key_auth(
     app: FastAPI,
     client: TestClient,
-    endpoint: str,
     auth: compose.httpx.HeaderAPIKeyAuth,
     expected_status_code: int,
 ):
-    response = client.get(endpoint, auth=auth)
+    response = client.get("/items", auth=auth)
 
     assert response.status_code == expected_status_code
 
 
 def test_header_api_key_auth_missing(app: FastAPI, client: TestClient):
-    response = client.get("/items/single-key-auth")
+    response = client.get("/items")
 
     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
