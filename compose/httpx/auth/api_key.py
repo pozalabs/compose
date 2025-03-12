3ba8942e54b2243820de01bc06a24a85910a7e37
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-12 17:42:36 +0900

    IP-6287: [compose] httpx 다중 키 헤더 인증 구현 (#370)
    Revert "IP-6287: [compose] httpx 다중 키 헤더 인증 구현 (#369)"
    
    This reverts commit d88c2b625f721ee38c2a55a91816a28f22b972ac.

--- compose/httpx/auth/api_key.py
+++ compose/httpx/auth/api_key.py
@@ -1,18 +1,13 @@
 from collections.abc import Generator
-from typing import Self
 
 import httpx
 
 
 class HeaderAPIKeyAuth(httpx.Auth):
-    def __init__(self, secrets: dict[str, str]) -> None:
-        self.secrets = secrets
+    def __init__(self, api_key: str, header_name: str = "X-API-Key"):
+        self.api_key = api_key
+        self.header_name = header_name
 
     def auth_flow(self, request: httpx.Request) -> Generator[httpx.Request, httpx.Response, None]:
-        for key, value in self.secrets.items():
-            request.headers[key] = value
+        request.headers[self.header_name] = self.api_key
         yield request
-
-    @classmethod
-    def single(cls, key: str, header_name: str = "x-api-key") -> Self:
-        return cls({header_name: key})
