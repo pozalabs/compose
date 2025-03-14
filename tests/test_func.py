b009b1ddfcc065dca3627e817adcfbc05dd6600a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-14 16:55:03 +0900

    IP-6305: [compose] 자주 쓰는 함수형 기능 제공 (#374)
    * c: func 모듈 구현
    
    * c: some_or_none 구현
    
    * c: unwrap 구현

---
+++ tests/test_func.py
@@ -0,0 +1,49 @@
+from collections.abc import Iterable
+
+import pytest
+
+import compose
+
+
+@pytest.mark.parametrize(
+    "iterable, expected",
+    [
+        ((i for i in [1, 2, 3] if i == 1), 1),
+        ((i for i in [1, 2, 3] if i == 4), None),
+        ((i for i in [1, 2, 3] if i == 1), 1),
+        ((i for i in [1, 2, 3] if i == 4), None),
+        ([1, 2, 3], 1),
+        ([], None),
+    ],
+    ids=(
+        "일치하는 값을 리턴할 수 있는 제너레이터",
+        "일치하는 값을 리턴할 수 없는 제너레이터",
+        "일치하는 값을 리턴할 수 있는 리스트 컴프리헨션",
+        "일치하는 값을 리턴할 수 없는 리스트 컴프리헨션",
+        "일치하는 값을 리턴할 수 있는 리스트",
+        "일치하는 값을 리턴할 수 없는 리스트",
+    ),
+)
+def test_some_or_none[T](iterable: Iterable[T], expected: T | None):
+    assert compose.func.some_or_none(iterable) == expected
+
+
+@pytest.mark.parametrize(
+    "v, expected",
+    [(1, 1)],
+)
+def test_can_unwrap[T](v: T | None, expected: T):
+    assert compose.func.unwrap(v, ValueError("Unexpected value")) == expected
+
+
+@pytest.mark.parametrize(
+    "v, exc",
+    [
+        (None, ValueError("Unexpected value")),
+    ],
+)
+def test_cannot_unwrap[T](v: T | None, exc: Exception):
+    with pytest.raises(type(exc)) as exc_info:
+        compose.func.unwrap(v, exc)
+
+    assert exc is exc_info.value
