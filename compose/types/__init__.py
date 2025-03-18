7aa610cab3ea4feb54a806f969f04ad739104337
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-18 14:36:37 +0900

    u: vo -> primitive 모듈명 변경

--- compose/types/__init__.py
+++ compose/types/__init__.py
@@ -2,8 +2,8 @@ from .datetime import DateRange, DateTime
 from .duration import Seconds
 from .helper import CoreSchemaGettable, SupportsGetValidators, chain, get_pydantic_core_schema
 from .object_id import PyObjectId
+from .primitive import Float, Int, IntList, Str, StrList, TypedList
 from .url import S3ContentUrl
-from .vo import Float, Int, IntList, Str, StrList, TypedList
 from .web import ContentDisposition
 
 __all__ = [
