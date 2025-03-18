bb17991deea808db008e4c440f0fb15a69c81d97
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-18 14:29:32 +0900

    c: Seconds 구현

---
+++ compose/types/duration.py
@@ -0,0 +1,22 @@
+from typing import Any, Self
+
+from . import vo
+
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE
+
+
+class Seconds(vo.Int):
+    def __new__(cls, v: Any, /) -> Self:
+        v = super().__new__(cls, v)
+        if v < 0:
+            raise ValueError("`Seconds` must be a non-negative integer")
+        return v
+
+    @classmethod
+    def from_hours(cls, hours: float) -> Self:
+        return cls(hours * SECONDS_PER_HOUR)
+
+    @classmethod
+    def from_minutes(cls, minutes: float) -> Self:
+        return cls(minutes * SECONDS_PER_MINUTE)
