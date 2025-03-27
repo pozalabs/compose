78ac05802ed6a4492593a178dd263d0d9d5a6b94
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-03-27 17:42:23 +0900

    u: 테스트 보완

--- tests/fastapi/unit/test_offset_pagination_params.py
+++ tests/fastapi/unit/test_offset_pagination_params.py
@@ -1,5 +1,6 @@
 import http
 
+import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
@@ -16,8 +17,16 @@ def get(pagination_params: compose.fastapi.OffsetPaginationParams.as_depends()):
 client = TestClient(app)
 
 
-def test_pagination_params_injected():
-    response = client.get(url="/items")
+@pytest.mark.parametrize(
+    "params, expected",
+    [
+        (None, dict(page=1, per_page=10)),
+        (dict(page=1, per_page=20), dict(page=1, per_page=20)),
+        (dict(page=2, per_page=10), dict(page=2, per_page=10)),
+    ],
+)
+def test_pagination_params_injected(params: dict[str, int] | None, expected: dict[str, int]):
+    response = client.get(url="/items", params=params)
 
     assert response.status_code == http.HTTPStatus.OK
-    assert response.json() == {"page": 1, "per_page": 10}
+    assert response.json() == expected
