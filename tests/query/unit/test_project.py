c7c25a71d6c9c1141efe2ffa1e2ae291fc8926cf
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-05-02 11:40:59 +0900

    IP-6462: [compose] Project 초기화 편의 메서드 추가 (#391)
    * u: Project.from_ 메서드 구현
    
    * u: None 불가 조건 추가

--- tests/query/unit/test_project.py
+++ tests/query/unit/test_project.py
@@ -39,6 +39,26 @@ from compose.query.mongo.op import Project
                 }
             },
         ),
+        (
+            Project.from_(
+                include=["field1"],
+                exclude=["field2"],
+                ref={"field3": "$field3", "field4": "$ref.field4"},
+            ),
+            {
+                "$project": {
+                    "field1": 1,
+                    "field2": 0,
+                    "field3": "$field3",
+                    "field4": "$ref.field4",
+                }
+            },
+        ),
+    ],
+    ids=[
+        "spec",
+        "from_attrs",
+        "from_",
     ],
 )
 def test_expression(project: op.Project, expected: op.DictExpression):
