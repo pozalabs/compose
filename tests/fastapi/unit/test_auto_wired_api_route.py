4804a62e2d32eb0cae6bf75dc100202562a87289
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-12-04 22:37:13 +0900

    c: 테스트 작성

---
+++ tests/fastapi/unit/test_auto_wired_api_route.py
@@ -0,0 +1,101 @@
+import http
+from collections.abc import Callable
+from typing import Any
+
+import pytest
+from dependency_injector import providers
+from fastapi import APIRouter, FastAPI
+from pydantic import Field
+from starlette.testclient import TestClient
+
+import compose
+
+
+class User(compose.BaseModel):
+    name: str
+
+
+class ListUsers(compose.query.Query):
+    names: list[str] = Field(default_factory=list, alias="name")
+
+    def to_query(self) -> Callable[[User], bool]:
+        def filter_(user: User) -> bool:
+            return user.name in self.names
+
+        return filter_
+
+
+class UserRepository:
+    def __init__(self):
+        self._items = [
+            User(name="user1"),
+            User(name="user2"),
+        ]
+
+    def find_by_name(self, name: str) -> User | None:
+        return next((item for item in self._items if item.name == name), None)
+
+    def filter(self, qry: ListUsers) -> list[User]:
+        filter_users = qry.to_query()
+        return [item for item in self._items if filter_users(item)]
+
+
+class UserContainer(compose.dependency.DeclarativeContainer):
+    user_repository = providers.Singleton(UserRepository)
+
+
+class ApplicationContainer(compose.dependency.DeclarativeContainer):
+    user = providers.Container(UserContainer)
+
+
+provide = compose.dependency.create_provider(ApplicationContainer)
+router = APIRouter(route_class=compose.fastapi.create_auto_wired_route(provide))
+
+
+@router.get("/v1/users/{name}", response_model=User)
+def retrieve_user(
+    name: str,
+    user_repository: UserRepository,
+):
+    return user_repository.find_by_name(name)
+
+
+@pytest.fixture
+def container():
+    wirer = compose.dependency.create_wirer(packages=[])
+    container = ApplicationContainer.wired(wirer=wirer, modules=[__name__])
+    yield container
+    container.unwire()
+
+
+# NOTE: 라우터를 사용지 않고 `app.get`으로 엔드포인트를 바로 등록하면 의존성이 주입되지 않음
+@pytest.fixture
+def app(container: ApplicationContainer) -> FastAPI:
+    app = FastAPI()
+    app.container = container
+    app.include_router(router)
+    return app
+
+
+@pytest.fixture
+def client(app: FastAPI) -> TestClient:
+    return TestClient(app)
+
+
+@pytest.mark.parametrize(
+    "name, expected_status_code, expected_response",
+    [
+        ("user1", http.HTTPStatus.OK, {"name": "user1"}),
+        ("user2", http.HTTPStatus.OK, {"name": "user2"}),
+    ],
+)
+def test_route(
+    client: TestClient,
+    name: str,
+    expected_status_code: int,
+    expected_response: dict[str, Any],
+):
+    response = client.get(f"/v1/users/{name}")
+
+    assert response.status_code == expected_status_code
+    assert response.json() == expected_response
