4184a538e8b59226f2d5087f86395f295fff4142
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2026-02-11 11:27:39 +0900

    u: Event를 Id 타입 제네릭으로 변경하고 MongoEvent 조건부 정의

--- compose/event/__init__.py
+++ compose/event/__init__.py
@@ -1,3 +1,10 @@
 from .event import Event
 
 __all__ = ["Event"]
+
+try:
+    from .event import MongoEvent
+
+    __all__ += ["MongoEvent"]
+except ImportError:
+    pass
--- compose/event/event.py
+++ compose/event/event.py
@@ -1,6 +1,20 @@
+from typing import Generic, TypeVar
+
 from .. import container, field, types
 
+IdT = TypeVar("IdT")
 
-class Event(container.BaseModel):
-    id: types.PyObjectId = field.IdField(default_factory=types.PyObjectId)
+
+class Event(container.BaseModel, Generic[IdT]):
+    id: IdT
     published_at: types.DateTime = field.DateTimeField()
+
+
+try:
+    from ..types import PyObjectId
+
+    class MongoEvent(Event[PyObjectId]):
+        id: PyObjectId = field.IdField(default_factory=PyObjectId)
+
+except ImportError:
+    pass
