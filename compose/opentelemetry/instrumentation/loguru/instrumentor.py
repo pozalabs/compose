c175652b56bddf474d342f21fe169ee821e45b09
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2026-02-13 17:03:09 +0900

    IP-6975: [compose] loguru instrumentor의 해제 버그 수정 및 불필요한 간접 계층 제거 (#477)

--- compose/opentelemetry/instrumentation/loguru/instrumentor.py
+++ compose/opentelemetry/instrumentation/loguru/instrumentor.py
@@ -13,68 +13,61 @@ ORIGINAL_PATCHER_ATTR = "_otel_original_patcher"
 _instruments = tuple()
 
 if TYPE_CHECKING:
-    from loguru import Logger
+    from loguru import Logger, Record
 
 
-def trace_injector(
-    tracer_provider: trace.TracerProvider,
-) -> Callable[[dict[str, Any]], dict[str, Any]]:
-    def inject_trace(record: dict[str, Any]) -> dict[str, Any]:
+RecordPatcher = Callable[["Record"], None]
+
+
+def create_trace_patcher(tracer_provider: trace.TracerProvider) -> RecordPatcher:
+    resource = getattr(tracer_provider, "resource", None)
+    service_name = resource.attributes.get("service.name", "") if resource is not None else ""
+
+    def patcher(record: Record) -> None:
         span = trace.get_current_span()
         ctx = span.get_span_context()
         is_valid_span = span != trace.INVALID_SPAN
 
-        resource = getattr(tracer_provider, "resource", None)
-        trace_record = {
-            "otel_service_name": (
-                resource.attributes.get("service.name", "") if resource is not None else ""
-            ),
-            "otel_trace_id": (
-                trace.format_trace_id(ctx.trace_id)
-                if is_valid_span
-                else str(trace.INVALID_TRACE_ID)
-            ),
-            "otel_span_id": (
-                trace.format_span_id(ctx.span_id) if is_valid_span else str(trace.INVALID_SPAN_ID)
-            ),
-            "otel_trace_sampled": ctx.trace_flags.sampled if is_valid_span else False,
-        }
-
-        extra = record.get("extra", {})
-        return {"extra": extra | trace_record}
-
-    return inject_trace
-
-
-def create_record_patcher(
-    inject_trace: Callable[[dict[str, Any]], dict[str, Any]],
-) -> Callable[[dict[str, Any]], None]:
-    def patcher(record: dict[str, Any]) -> None:
-        record |= inject_trace(record)
+        record["extra"].update(
+            {
+                "otel_service_name": service_name,
+                "otel_trace_id": (
+                    trace.format_trace_id(ctx.trace_id)
+                    if is_valid_span
+                    else str(trace.INVALID_TRACE_ID)
+                ),
+                "otel_span_id": (
+                    trace.format_span_id(ctx.span_id)
+                    if is_valid_span
+                    else str(trace.INVALID_SPAN_ID)
+                ),
+                "otel_trace_sampled": ctx.trace_flags.sampled if is_valid_span else False,
+            }
+        )
 
     return patcher
 
 
 def create_configure_wrapper(
-    inject_trace: Callable[[dict[str, Any]], dict[str, Any]],
+    trace_patcher: RecordPatcher,
 ) -> Callable[..., list[int]]:
     def wrapped_configure(
         func: Callable[..., list[int]],
         instance: Logger,
-        *args,
-        **kwargs,
+        args: tuple[Any],
+        kwargs: dict[str, Any],
     ) -> list[int]:
-        """https://github.com/DataDog/dd-trace-py/blob/main/ddtrace/contrib/loguru/patch.py"""
-
         original_patcher = kwargs.get("patcher")
         setattr(instance, ORIGINAL_PATCHER_ATTR, original_patcher)
 
         if original_patcher is None:
             return func(*args, **kwargs)
 
-        def wrapped_patcher(record: dict[str, Any]) -> None:
-            original_patcher(record)
-            record |= inject_trace(record)
+        user_patcher: RecordPatcher = original_patcher
+
+        def wrapped_patcher(record: Record) -> None:
+            user_patcher(record)
+            trace_patcher(record)
 
         kwargs["patcher"] = wrapped_patcher
         return func(*args, **kwargs)
@@ -82,15 +75,13 @@ def create_configure_wrapper(
     return wrapped_configure
 
 
-def default_record_patcher(_: dict[str, Any]) -> None:
+def _default_record_patcher(_: Record) -> None:
     return None
 
 
 class LoguruInstrumentor(BaseInstrumentor):
     """
-
     Reference:
-        https://github.com/DataDog/dd-trace-py/tree/main/ddtrace/contrib/loguru
         https://github.com/open-telemetry/opentelemetry-python-contrib/tree/main/instrumentation/opentelemetry-instrumentation-logging
     """
 
@@ -100,20 +91,17 @@ class LoguruInstrumentor(BaseInstrumentor):
     def _instrument(self, **kwargs) -> None:
         tracer_provider = kwargs.get("tracer_provider", trace.get_tracer_provider())
 
-        inject_trace = trace_injector(tracer_provider)
-        logger.configure(patcher=create_record_patcher(inject_trace))
+        trace_patcher = create_trace_patcher(tracer_provider)
+        logger.configure(patcher=trace_patcher)
         wrapt.wrap_function_wrapper(
             logger,
             "configure",
-            create_configure_wrapper(inject_trace),
+            create_configure_wrapper(trace_patcher),
         )
 
     def _uninstrument(self, **kwargs) -> None:
         original_patcher = getattr(logger, ORIGINAL_PATCHER_ATTR, None)
-        has_original_patcher = original_patcher is not None
-        if has_original_patcher:
-            logger.configure(patcher=original_patcher)
-
         unwrap(logger, "configure")
-        if not has_original_patcher:
-            logger.configure(patcher=default_record_patcher)
+        logger.configure(patcher=original_patcher or _default_record_patcher)
+        if hasattr(logger, ORIGINAL_PATCHER_ATTR):
+            delattr(logger, ORIGINAL_PATCHER_ATTR)
