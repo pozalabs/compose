01b8076ce8f75ec4329544123ef54f758dbab07c
KIM Sunwoong <35526032+novdov@users.noreply.github.com>
2026-03-04 12:19:55 +0900

    IP-7035: [compose] S3 CRUD 래퍼 공통 클래스 제공 (#532)

--- compose/aws/__init__.py
+++ compose/aws/__init__.py
@@ -1,4 +1,5 @@
+from .object_store import S3ObjectStore
 from .s3 import s3_obj_exists
 from .url_generator import S3UrlGenerator
 
-__all__ = ["S3UrlGenerator", "s3_obj_exists"]
+__all__ = ["S3ObjectStore", "S3UrlGenerator", "s3_obj_exists"]
