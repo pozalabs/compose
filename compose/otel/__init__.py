4924103763aebee5d8663956dbc629cbd88a708a
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2026-06-19 11:34:46 +0900

    r: 에러 메시지 간결화

--- compose/otel/__init__.py
+++ compose/otel/__init__.py
@@ -2,9 +2,6 @@ try:
     from .instrumentation.loguru.instrumentor import LoguruInstrumentor
     from .tracer_provider import ServiceResourceAttrs, get_default_tracer_provider
 except ImportError:
-    raise ImportError(
-        "Install 'opentelemetry-api', 'opentelemetry-sdk',"
-        " and 'opentelemetry-exporter-otlp-proto-http' to use otel features"
-    ) from None
+    raise ImportError("Install the 'otel' extra to use otel features") from None
 
 __all__ = ["LoguruInstrumentor", "ServiceResourceAttrs", "get_default_tracer_provider"]
