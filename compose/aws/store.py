9276082ea1fdccfcf5d8f34472bc931f8f74aaca
Kim Sunwoong <35526032+novdov@users.noreply.github.com>
2026-08-03 10:44:46 +0900

    S3Store 타입 힌트 강화 (#625)

--- compose/aws/store.py
+++ compose/aws/store.py
@@ -6,33 +6,41 @@ import botocore.exceptions
 
 if TYPE_CHECKING:
     import mypy_boto3_s3
+    from mypy_boto3_s3.type_defs import CopySourceTypeDef, PutObjectRequestTypeDef
 
 
 class S3Store:
     def __init__(self, s3_client: mypy_boto3_s3.S3Client, bucket: str) -> None:
-        self._client = s3_client
-        self._bucket = bucket
+        self.client = s3_client
+        self.bucket = bucket
 
-    def upload(self, key: str, body: bytes | IO[bytes], **params: Any) -> None:
-        self._client.put_object(Bucket=self._bucket, Key=key, Body=body, **params)
+    def upload(self, key: str, body: bytes | IO[bytes], **kwargs: Any) -> None:
+        params: PutObjectRequestTypeDef = {
+            "Bucket": self.bucket,
+            "Key": key,
+            "Body": body,
+            **kwargs,
+        }
+        self.client.put_object(**params)
 
     def download(self, key: str) -> bytes:
-        response = self._client.get_object(Bucket=self._bucket, Key=key)
+        response = self.client.get_object(Bucket=self.bucket, Key=key)
         return response["Body"].read()
 
     def delete(self, key: str) -> None:
-        self._client.delete_object(Bucket=self._bucket, Key=key)
+        self.client.delete_object(Bucket=self.bucket, Key=key)
 
     def copy(self, src_key: str, dst_key: str) -> None:
-        self._client.copy_object(
-            Bucket=self._bucket,
+        copy_source: CopySourceTypeDef = {"Bucket": self.bucket, "Key": src_key}
+        self.client.copy_object(
+            Bucket=self.bucket,
             Key=dst_key,
-            CopySource={"Bucket": self._bucket, "Key": src_key},
+            CopySource=copy_source,
         )
 
     def exists(self, key: str) -> bool:
         try:
-            self._client.head_object(Bucket=self._bucket, Key=key)
+            self.client.head_object(Bucket=self.bucket, Key=key)
             return True
         except botocore.exceptions.ClientError as exc:
             if exc.response["Error"]["Code"] == "404":
